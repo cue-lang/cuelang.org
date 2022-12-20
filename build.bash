d2b1a8e8111a7dd79995309f1f6fb5237035ea35
Paul Jolly <paul@myitcv.io>
2022-12-20 15:05:11 +0000

    netlify: remove unused variable from build script
    Simplifies later changes, and we can more clearly see whether it breaks
    something.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>

--- build.bash
+++ build.bash
@@ -10,14 +10,11 @@ then
 	exit 1
 fi
 
-env="production"
-
 # If we are running on netlify (i.e. a deploy) and we are building tip then we
 # need to grab the master of our cuelang.org/go and main from
 # github.com/cue-sh/playground dependencies
 if [ "$BRANCH" = "tip" ]
 then
-	env="tip"
 	GOPROXY=direct go get cuelang.org/go@master
 	# Now force cuelang.org/go  through the proxy so that the /pkg.go.dev redirect works
 	go get -d cuelang.org/go@$(go list -m -f={{.Version}} cuelang.org/go)
