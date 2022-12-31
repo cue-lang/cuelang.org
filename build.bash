3740e12862fa572dc660a985cd53740262e07c19
Paul Jolly <paul@myitcv.io>
2022-12-31 21:12:39 +0000

    all: time build commands
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I74f089d6a3f840f10e8bde036edaafbf1115728c

--- build.bash
+++ build.bash
@@ -4,21 +4,24 @@ set -eux
 
 # build.bash is used to build the site ready for deploy.
 
+time="time -p"
+
 # If we are building tip then we need to grab the master of our cuelang.org/go
 # and main from github.com/cue-sh/playground dependencies
 if [ "${BRANCH:-}" = "tip" ]
 then
-	GOPROXY=direct go get cuelang.org/go@master
+	GOPROXY=direct $time go get cuelang.org/go@master
 	# Now force cuelang.org/go  through the proxy so that the /pkg.go.dev redirect works
-	go get -d cuelang.org/go@$(go list -m -f={{.Version}} cuelang.org/go)
-	go mod tidy
-	go generate ./...
+	$time go get -d cuelang.org/go@$(go list -m -f={{.Version}} cuelang.org/go)
+	$time go mod tidy
+	$time go generate ./...
 fi
 
+echo $SHELL
 # Main site
-git submodule update -f --init --recursive
-npm install
-hugo $@
+$time git submodule update -f --init --recursive
+$time npm install
+$time hugo $@
 
 # CUE playground
 bash play/build.bash
