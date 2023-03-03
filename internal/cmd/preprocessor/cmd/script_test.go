c4e40d4a44794bd4e0e03690f8face987b0a7aef
Paul Jolly <paul@myitcv.io>
2023-03-03 12:49:25 +0000

    internal/cmd/preprocessor: initial commit
    Create a boilerplate command using core.go copied from the main CUE
    repo.
    
    For now execute does absolutely nothing, other than provide the option
    of a --debug flag.
    
    Also put in place a testscript-based test setup ready for later use.
    This includes the definition of find, a command similar to unix find,
    that lists non-golden files in a directory structure. This helps with
    assertions on what execute has actually done to file structure, with cmp
    being use to then assert file contents versus golden files in the usual
    way.
    
    Add a simple test that asserts on the output of --help.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ib6800c999476f45d3370e5d584a3355748c2fb02

---
+++ internal/cmd/preprocessor/cmd/script_test.go
@@ -0,0 +1,65 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(testscript.RunMain(m, map[string]func() int{
+		"preprocessor": Main,
+		"cmd-find":     findCmd,
+	}))
+}
+
+func TestScripts(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		UpdateScripts: os.Getenv("CUE_UPDATE") != "",
+		Dir:           "testdata",
+	})
+}
+
+// findCmd is rather like Unix find. It is used to list files and directories
+// contained by the working directory. By default neither .golden files nor
+// directoriy paths are listed.
+func findCmd() int {
+	fDir := flag.Bool("dir", false, "print directory paths")
+	fGolden := flag.Bool("golden", false, "print .golden files")
+	flag.Parse()
+
+	filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
+		var printEntry bool
+		if d.IsDir() {
+			printEntry = *fDir
+		} else {
+			isGolden := filepath.Ext(d.Name()) == ".golden"
+			printEntry = !isGolden || *fGolden
+		}
+		if printEntry {
+			fmt.Printf("%s\n", path)
+		}
+		return nil
+	})
+
+	return 0
+}
