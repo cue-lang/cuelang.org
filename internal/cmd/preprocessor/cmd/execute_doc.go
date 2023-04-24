c8b5868bc409034754a8ed118d3ea1f6039832f9
Paul Jolly <paul@myitcv.io>
2023-04-24 15:50:11 +0100

    preprocessor: implement watching
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Iee64ca384f25726ee7d4ac001bc93a904fff1fd5

--- internal/cmd/preprocessor/cmd/execute_doc.go
+++ internal/cmd/preprocessor/cmd/execute_doc.go
@@ -154,5 +154,6 @@ func newExecuteCmd(c *Command) *cobra.Command {
 	}
 	cmd.Flags().Bool(string(flagDebug), false, "turn on debug logging")
 	cmd.Flags().StringP(string(flagDir), "d", "", "working directory. Defaults to the current working directory")
+	cmd.Flags().BoolP(string(flagServe), "s", false, "watch the working directory for changes, executing on each batch of changes")
 	return cmd
 }
