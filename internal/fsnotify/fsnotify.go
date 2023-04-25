8ed60129718f4d61da9ca0adc752773b7885a305
Paul Jolly <paul@myitcv.io>
2023-04-25 09:10:44 +0100

    internal/fsnotify: initial commit
    Package fsnotify is a light wrapper around github.com/fsnotify/fsnotify
    that allows for recursively watching directories, and provides a simple
    wrapper for batching events.
    
    This is used in a later CL where we implement file watching in the
    preprocessor.
    
    Note this also bumps the version of github.com/rogpeppe/go-internal in
    order to use a change to testscript in which we expose
    (*TestScript).stdout via Stdout() and expose (*TestScript).stderr via
    Stderr().
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ia117d2b45d7e398ec0d31ca1a86e55d536628f6b
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/553235
    TryBot-Result: CUEcueckoo <cueckoo+gerrithub@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

---
+++ internal/fsnotify/fsnotify.go
@@ -0,0 +1,51 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package fsnotify is a light wrapper around github.com/fsnotify/fsnotify that
+// allows for recursively watching directories, and provides a simple wrapper
+// for batching events.
+package fsnotify
+
+import (
+	"io"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+// Provide access to the underlying fsnotify ops.
+const (
+	Chmod  = fsnotify.Chmod
+	Create = fsnotify.Create
+	Remove = fsnotify.Remove
+	Rename = fsnotify.Rename
+	Write  = fsnotify.Write
+)
+
+type Event = fsnotify.Event
+
+// Option is used to further configure a Watcher or BatchedWatcher.
+type Option option
+
+type option func(p *options)
+
+type options struct {
+	debug io.Writer
+}
+
+// Debug configures debug-level logging via the io.Writer w.
+func Debug(w io.Writer) Option {
+	return func(o *options) {
+		o.debug = w
+	}
+}
