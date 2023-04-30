78aa1025825dbecfad3094c8bc0e005176038f04
Paul Jolly <paul@myitcv.io>
2023-04-30 05:23:48 +0100

    preprocessor: properly process root files
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I440375cd8f6fac2af778f9ed5ab85ab188989dbf

--- internal/cmd/preprocessor/cmd/rootfile.go
+++ internal/cmd/preprocessor/cmd/rootfile.go
@@ -14,7 +14,20 @@
 
 package cmd
 
-import "path/filepath"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cue-lang/cuelang.org/internal/parse"
+)
+
+var (
+	withFunctions = map[string]any{
+		"sidebyside": true,
+	}
+)
 
 // rootFile is a convenience data structure for keeping track of root files we
 // have found.
@@ -34,6 +47,17 @@ type rootFile struct {
 
 	// lang is the language of the root file filename
 	lang lang
+
+	// contents is the raw []byte contents of the file captured
+	// during parse in order that the subsequent write back to
+	// the source file can be skipped if the contents are byte-equal.
+	contents []byte
+
+	// header is the Yaml header of a root file
+	header []byte
+
+	// body is the text/template/parse-d result of the body
+	body *parse.Tree
 }
 
 // newRootFile creates a new rootFile for fn.
@@ -48,6 +72,131 @@ func (p *page) newRootFile(fn string, lang lang, prefix, ext string) *rootFile {
 }
 
 func (rf *rootFile) process(sourcePath, targetPath string) error {
-	// For now we just copy from source to target
+	// For now, we only support en as a main language. For other languages
+	// we simply copy from source to target.
+	if rf.lang != langEn {
+		return copyFile(sourcePath, targetPath)
+	}
+	// HTML files don't need any special processing
+	if rf.ext == "html" {
+		return copyFile(sourcePath, targetPath)
+	}
+
+	// Start by parsing the root file
+	if err := rf.parse(); err != nil {
+		return fmt.Errorf("failed to parse %s: %w", rf.filename, err)
+	}
+
+	// Write the parsed rootFile back to ensure we have have normalised input.
+	writeBack := new(bytes.Buffer)
+	if err := rf.write(writeBack); err != nil {
+		return fmt.Errorf("failed to write to %s: %w", sourcePath, err)
+	}
+	if err := writeIfDiff(writeBack, sourcePath, rf.contents); err != nil {
+		return fmt.Errorf("failed to write normalised input: %w", err)
+	}
+
+	// Transform the root file
+	transformed := new(bytes.Buffer)
+	if err := rf.transform(transformed); err != nil {
+		return fmt.Errorf("failed to transform: %w", err)
+	}
+	if err := writeIfDiff(transformed, targetPath, nil); err != nil {
+		return fmt.Errorf("failed to write transformed output: %w", err)
+	}
+
 	return copyFile(sourcePath, targetPath)
+}
+
+func (rf *rootFile) transform(b *bytes.Buffer) error {
+	if err := rf.write(b); err != nil {
+		return err
+	}
+	return nil
+}
+
+var (
+	headerStart = []byte("---\n")   // clear line
+	headerEnd   = []byte("\n---\n") // clear line required
+)
+
+// parse parses a root file into its components.
+func (rf *rootFile) parse() error {
+	f, err := os.ReadFile(rf.filename)
+	if err != nil {
+		return fmt.Errorf("failed to read: %w", err)
+	}
+	rf.contents = f
+	if !bytes.HasPrefix(f, headerStart) {
+		return fmt.Errorf("failed to find start of header")
+	}
+	header := f[len(headerStart):]
+	body := header
+	if bytes.HasPrefix(header, headerStart) {
+		// Special case of empty header
+		header = nil
+		body = body[len(headerStart):]
+	} else {
+		end := bytes.Index(header, headerEnd)
+		if end == -1 {
+			return fmt.Errorf("failed to find end of header")
+		}
+		header = header[:end]
+		body = body[end+len(headerEnd):]
+	}
+	rf.header = header
+
+	parseTrees, err := parse.Parse(rf.filename, string(body), "{{{", "}}}", withFunctions)
+	if err != nil {
+		return fmt.Errorf("failed to parse body: %w", err)
+	}
+	rf.body = parseTrees[rf.filename]
+
+	return nil
+}
+
+// write writes a parsed rf to the absolute file target. It panics if rf
+// has not been parsed, a state that is indicated by a nil body.
+func (rf *rootFile) write(b *bytes.Buffer) error {
+	if rf.body == nil {
+		panic("cannot call write on an unparsed rootFile")
+	}
+	b.Write(headerStart)
+	if len(rf.header) == 0 {
+		b.Write(headerStart)
+	} else {
+		b.Write(rf.header)
+		b.Write(headerEnd)
+	}
+
+	for _, n := range rf.body.Root.Nodes {
+		fmt.Fprintf(b, "%v", n)
+	}
+
+	return nil
+}
+
+func writeIfDiff(b *bytes.Buffer, target string, curVal []byte) error {
+	var err error
+	var exists bool
+	if curVal == nil {
+		// The file might not exist
+		curVal, err = os.ReadFile(target)
+		if err == nil {
+			exists = true
+		}
+	}
+	// Early return if the target exists and is byte-equal
+	if exists && bytes.Equal(curVal, b.Bytes()) {
+		return nil
+	}
+	targetDir := filepath.Dir(target)
+	if err := os.MkdirAll(targetDir, 0777); err != nil {
+		return fmt.Errorf("failed to ensure %s exists: %w", targetDir, err)
+	}
+	if err := os.WriteFile(target, b.Bytes(), 0666); err != nil {
+		return fmt.Errorf("failed to write to %s: %w", target, err)
+	}
+	return nil
+
 }
