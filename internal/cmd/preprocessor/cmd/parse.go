ece721ff7ae44badc043f732627b756168f3081e
Paul Jolly <paul@myitcv.io>
2023-04-30 05:23:48 +0100

    preprocessor: implement basic transform in execute
    This CL implements a basic transformation of human-friendly /content
    into Hugo-friendly shortcodes.
    
    The running of execute now happens in phases:
    
    - parse: parse root files in /content into an AST-like representation of
      each.
    - run: for root files that contain scripts/equivalent, run these to
      ensure they are current.
    - write back: write root files back to the /content source.
    - transform: write root files to their /hugo/content versions.
    
    The parse and run steps have the effect of normalising the /content
    inputs. The subsequent write back ensures we persist the normalised
    versions on disk for consistency (consider this the equivalent of go fmt
    for /content files).
    
    For now the run step only formats some of the files in {{{ with }}}
    txtar archives. A later CL will implement a full "run" of each
    testscript. This will then mean that updated txtar archives get written
    back to source /content files during the write back.
    
    The only text/template node implemented for now is:
    
      {{{ with sidebyside ... }}}
    
    But we have the basic infrastructure in place to fully support other
    directives at a later date.
    
    Error handling approach will be improved in later CLs, once we establish
    a clearer idea of how/where errors should be reported and when they are
    fatal.
    
    Changes in /content in this CL are the result of normalised inputs being
    written back, post import and run phases.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I440375cd8f6fac2af778f9ed5ab85ab188989dbf

---
+++ internal/cmd/preprocessor/cmd/parse.go
@@ -0,0 +1,252 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/cue-lang/cuelang.org/internal/parse"
+	"github.com/rogpeppe/go-internal/txtar"
+)
+
+const (
+	// markdown header delimiters
+	headerDelimiter = "---"
+	headerLine      = headerDelimiter + "\n"
+
+	// {{with}} blocks can optionally be wrapped in a ```coq ``` pair
+	// to make rendering nicer within tools like Notion
+	withStartWrapperLine = "```coq\n" // could appear at the start or a clear line
+	withEndWrapper       = "\n```\n"  // always appears on a clear line
+)
+
+// parse parses a root file into its component parse: a header and body. The body
+// is parsed as a text/template document.
+func (rf *rootFile) parse() error {
+	f, err := os.ReadFile(rf.filename)
+	if err != nil {
+		return fmt.Errorf("failed to read: %w", err)
+	}
+	rf.contents = f
+
+	bodyStart := 0
+
+	// Parse header. Must be right at the headerStart of the file
+	if !bytes.HasPrefix(f, []byte(headerLine)) {
+		return fmt.Errorf("failed to find start of header")
+	}
+	header := f[len(headerLine):]
+	bodyStart += len(headerLine)
+	// If the header is empty the end will immediately follow
+	if bytes.HasPrefix(header, []byte(headerLine)) {
+		header = nil
+		bodyStart += len(headerLine)
+	} else {
+		// It's not a prefix, it must be a clear line
+		const headerEnd = "\n" + headerLine
+		endIndex := bytes.Index(header, []byte(headerEnd))
+		if endIndex == -1 {
+			return fmt.Errorf("failed to find end of header")
+		}
+		bodyStart += endIndex + len(headerEnd)
+		header = header[:endIndex+1] // to leave the header including the \n
+	}
+	rf.header = header
+
+	// Prefix the body with as many newlines as we saw across
+	// the header block in order that offsets etc are accurate
+	rf.bodyPrefix = bytes.Repeat([]byte("\n"), bytes.Count(f[:bodyStart], []byte("\n")))
+	body := append(rf.bodyPrefix, f[bodyStart:]...)
+
+	// TODO derive the delimiters from the page's CUE config
+	parseTrees, err := parse.Parse(rf.filename, string(body), rf.page.leftDelim, rf.page.rightDelim, templateFunctions)
+	if err != nil {
+		return fmt.Errorf("failed to parse body: %w", err)
+	}
+	rf.body = parseTrees[rf.filename]
+	pc := parseContext{
+		rootFile: rf,
+		parts:    rf.body.Root.Nodes,
+	}
+
+	for ; pc.i < len(pc.parts); pc.i++ {
+		n := pc.parts[pc.i]
+		var newNode node
+		var err error
+		switch n := n.(type) {
+		case *parse.TextNode:
+			newNode, err = pc.parse_TextNode(n)
+		case *parse.WithNode:
+			newNode, err = pc.parse_WithNode(n)
+		case *parse.ActionNode:
+			newNode, err = pc.parse_ActionNode(n)
+		default:
+			return rf.bodyError(n, "do not know how to handle node of type %T", n)
+		}
+		if err != nil {
+			return err
+		}
+		rf.bodyParts = append(rf.bodyParts, newNode)
+	}
+
+	return nil
+}
+
+// bodyError is a convenience for creating a formatted error that includes the
+// position of n within the original input.
+func (rf *rootFile) bodyError(n parse.Node, format string, args ...any) error {
+	location, _ := rf.body.ErrorContext(n)
+	return fmt.Errorf(location+" "+format, args...)
+}
+
+// parseContext is a convenience for passing the current state of parsing of
+// the text/template/parse root node list. This is really only required to
+// handle the case of text nodes that may surround WithNode's. These nodes
+// need to have triple-backtick text stripped from them, and leading blank
+// lines in the case of the first text node.
+type parseContext struct {
+	*rootFile
+	parts []parse.Node
+	i     int
+}
+
+func (pc parseContext) parse_TextNode(n *parse.TextNode) (node, error) {
+	var lastWasWith, nextIsWith bool
+	if pc.i < len(pc.parts)-1 {
+		_, nextIsWith = pc.parts[pc.i+1].(*parse.WithNode)
+	}
+	if pc.i > 0 {
+		_, lastWasWith = pc.parts[pc.i-1].(*parse.WithNode)
+	}
+	t := string(n.Text)
+	if pc.i == 0 {
+		// We know this will be a text node because we put in a fake prefix
+		t = strings.TrimPrefix(t, string(pc.bodyPrefix))
+	}
+	if nextIsWith {
+		if t == withStartWrapperLine {
+			t = ""
+		} else if strings.HasSuffix(t, "\n"+withStartWrapperLine) {
+			t = strings.TrimSuffix(t, withStartWrapperLine)
+		}
+	}
+	if lastWasWith {
+		// the close ``` must always be on a clear line
+		t = strings.TrimPrefix(t, withEndWrapper)
+	}
+	return &textNode{
+		rf:   pc.rootFile,
+		text: []byte(t),
+	}, nil
+
+}
+
+func (pc parseContext) parse_WithNode(n *parse.WithNode) (node, error) {
+	// The only form we accept right now is {{{ with function "$lang" "label" }}}
+	// This is parsed as a command node with multiple args.
+	if n.ElseList != nil {
+		return nil, pc.bodyError(n.ElseList, "with node cannot have else block")
+	}
+	if len(n.Pipe.Cmds) != 1 {
+		return nil, pc.bodyError(n, "with node must comprise a single command")
+	}
+	c := n.Pipe.Cmds[0]
+	if l := len(c.Args); l == 0 {
+		return nil, pc.bodyError(c, "expected at least one arg, not zero")
+	}
+	arg0 := c.Args[0]
+	fn, ok := arg0.(*parse.IdentifierNode)
+	if !ok {
+		return nil, pc.bodyError(c, "expected identifier as first arg; got %T", arg0)
+	}
+	switch fn.Ident {
+	case "sidebyside":
+		return pc.parse_sidebyside(n, c.Args[1:])
+	case "sidetrack":
+	default:
+		return nil, pc.bodyError(fn, "do not know how to handle with identifier %q", fn.Ident)
+	}
+	return &nodeWrapper{
+		rf:         pc.rootFile,
+		underlying: n,
+	}, nil
+}
+
+func (pc parseContext) parse_sidebyside(n *parse.WithNode, args []parse.Node) (node, error) {
+	if len(args) != 2 {
+		return nil, pc.bodyError(n, "sidebyside requires two args")
+	}
+	var strArgs []string
+	for _, a := range args {
+		sa, ok := a.(*parse.StringNode)
+		if !ok {
+			return nil, pc.bodyError(a, "expected a string argument")
+		}
+		strArgs = append(strArgs, sa.Text)
+	}
+	lang, label := strArgs[0], strArgs[1]
+
+	// We only support a single TextNode body, i.e. the contents of a txtar archive
+	if n.List == nil || len(n.List.Nodes) != 1 {
+		return nil, pc.bodyError(n, "sidebyside must have a text-only body")
+	}
+	tn, ok := n.List.Nodes[0].(*parse.TextNode)
+	if !ok {
+		return nil, pc.bodyError(n, "sidebyside must have a text-only body")
+	}
+	// We "always" use {{{ with .. }}} on a clean line. Strip the leading \n that
+	// therefore forms part of the body.
+	text := tn.Text[1:]
+	ar := txtar.Parse(text)
+
+	res := &sidebysideNode{
+		rf:    pc.rootFile,
+		lang:  lang,
+		label: label,
+		ar:    ar,
+	}
+	return res, nil
+}
+
+func (pc parseContext) parse_ActionNode(n *parse.ActionNode) (node, error) {
+	// The only forms we support for now are those with a single command
+	// with a list of args, e.g. {{{TODO "note on embedding"}}}
+	if len(n.Pipe.Cmds) != 1 {
+		return nil, pc.bodyError(n, "action node must comprise a single command")
+	}
+	c := n.Pipe.Cmds[0]
+	if l := len(c.Args); l == 0 {
+		return nil, pc.bodyError(c, "expected at least one arg, not zero")
+	}
+	arg0 := c.Args[0]
+	fn, ok := arg0.(*parse.IdentifierNode)
+	if !ok {
+		return nil, pc.bodyError(c, "expected identifier as first arg; got %T", arg0)
+	}
+	switch fn.Ident {
+	case "TODO":
+	case "def":
+	case "reference":
+	default:
+		return nil, pc.bodyError(fn, "do not know how to handle with identifier %q", fn.Ident)
+	}
+	return &nodeWrapper{
+		rf:         pc.rootFile,
+		underlying: n,
+	}, nil
+}
