e785fd629697462947d2c8aa95d0223f7fe868ee
Paul Jolly <paul@myitcv.io>
2023-04-30 05:23:48 +0100

    preprocessor: implement basic transform in execute
    This CL implements a basic transformation of human-friendly /content
    into Hugo-friendly shortcodes.
    
    The running of execute now happens in phases:
    
    - parse: parse root files in /content into an AST-like representation of
      each.
    - run: for root files that contain scripts/equivalent, run these to
      ensure they are current.
    - write back: write root files back to the /content source.
    - transform: write root files to their /hugo/content versions.
    
    The parse and run steps have the effect of normalising the /content
    inputs. The subsequent write back ensures we persist the normalised
    versions on disk for consistency (consider this the equivalent of go fmt
    for /content files).
    
    For now the run step only formats some of the files in {{{ with }}}
    txtar archives. A later CL will implement a full "run" of each
    testscript. This will then mean that updated txtar archives get written
    back to source /content files during the write back.
    
    The only text/template node implemented for now is:
    
      {{{ with sidebyside ... }}}
    
    But we have the basic infrastructure in place to fully support other
    directives at a later date.
    
    Error handling approach will be improved in later CLs, once we establish
    a clearer idea of how/where errors should be reported and when they are
    fatal.
    
    Changes in /content in this CL are the result of normalised inputs being
    written back, post import and run phases.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I440375cd8f6fac2af778f9ed5ab85ab188989dbf
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/553298
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo+gerrithub@cuelang.org>

--- internal/cmd/preprocessor/cmd/rootfile.go
+++ internal/cmd/preprocessor/cmd/rootfile.go
@@ -14,7 +14,28 @@
 
 package cmd
 
-import "path/filepath"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cue-lang/cuelang.org/internal/parse"
+)
+
+var (
+	// templateFunctions is the list of bare {{with}} transforms that we
+	// support.  The variable is called templateFunctions because
+	// text/template/parse sees the bare words and interprets them as if they
+	// were function calls.
+	templateFunctions = map[string]any{
+		fnSidebyside: true,
+		"reference":  true,
+		"def":        true,
+		"sidetrack":  true,
+		"TODO":       true,
+	}
+)
 
 // rootFile is a convenience data structure for keeping track of root files we
 // have found.
@@ -34,6 +55,29 @@ type rootFile struct {
 
 	// lang is the language of the root file filename
 	lang lang
+
+	// contents is the raw []byte contents of the file captured
+	// during parse in order that the subsequent write back to
+	// the source file can be skipped if the contents are byte-equal.
+	contents []byte
+
+	// header is the Yaml header of a root file. It will either be nil
+	// or a \n terminated block (that should parse as yaml).
+	header []byte
+
+	// bodyPrefix is the newline padding added as a prefix to the string parsed
+	// as the body text/template. This helps ensure line numbers are accurate.
+	// This needs to be stripped off when writing out the result.
+	bodyPrefix []byte
+
+	// body is the text/template/parse-d result of the body
+	body *parse.Tree
+
+	// bodyParts is a list of the internalised versions of the
+	// text/template/parse-d input. Each node can be written back
+	// to the input format, run (to update itself), or written
+	// to the output format ready for consumption by Hugo.
+	bodyParts []node
 }
 
 // newRootFile creates a new rootFile for fn.
@@ -48,6 +92,119 @@ func (p *page) newRootFile(fn string, lang lang, prefix, ext string) *rootFile {
 }
 
 func (rf *rootFile) transform(targetPath string) error {
-	// For now we just copy from source to target
-	return copyFile(rf.filename, targetPath)
+	// For now, we only support en as a main language. For other languages
+	// we simply copy from source to target.
+	if rf.lang != langEn {
+		return copyFile(rf.filename, targetPath)
+	}
+	// HTML files don't need any special processing
+	if rf.ext == "html" {
+		return copyFile(rf.filename, targetPath)
+	}
+
+	// Start by parsing the root file
+	if err := rf.parse(); err != nil {
+		// Note errors in parse have position information
+		return fmt.Errorf("failed to parse: %w", err)
+	}
+
+	if err := rf.run(); err != nil {
+		return err
+	}
+
+	// Write the parsed rootFile back to ensure we have have normalised input.
+	writeBack := new(bytes.Buffer)
+	if err := rf.writeSource(writeBack); err != nil {
+		return fmt.Errorf("failed to write to %s: %w", rf.filename, err)
+	}
+	if err := writeIfDiff(writeBack, rf.filename, rf.contents); err != nil {
+		return fmt.Errorf("failed to write normalised input: %w", err)
+	}
+
+	// Transform the root file
+	transformed := new(bytes.Buffer)
+	if err := rf.writeTarget(transformed); err != nil {
+		return fmt.Errorf("failed to transform %s: %w", rf.filename, err)
+	}
+	if err := writeIfDiff(transformed, targetPath, nil); err != nil {
+		return fmt.Errorf("failed to write transformed output: %w", err)
+	}
+	return nil
+}
+
+// run is responsible for updating those nodes which contain inputs and outputs.
+// The inputs generally need to be formatted, scripts run, and outputs updated.
+// This method also adds as a means to check that we only have parsed things
+// that we can support.
+func (rf *rootFile) run() error {
+	for _, n := range rf.bodyParts {
+		if err := n.run(); err != nil {
+			return fmt.Errorf("failed to run %s: %w", rf.filename, err)
+		}
+	}
+	return nil
+}
+
+// transform writes out root file rf in a format that is ready to be consumed
+// by hugo.
+func (rf *rootFile) writeTarget(b *bytes.Buffer) error {
+	// Write out the header
+	b.WriteString(headerLine)
+	b.Write(rf.header)
+	b.WriteString(headerLine)
+	parts := rf.bodyParts
+
+	for _, n := range parts {
+		if err := n.writeTransformTo(b); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// write writes to b, in a form that would allow it to be parsed for a perfect
+// round trip. It panics if rf has not been parsed, a state that is indicated
+// by a nil body.
+func (rf *rootFile) writeSource(b *bytes.Buffer) error {
+	if rf.body == nil {
+		panic("cannot call write on an unparsed rootFile")
+	}
+	b.WriteString(headerLine)
+	b.Write(rf.header)
+	b.WriteString(headerLine)
+
+	for _, n := range rf.bodyParts {
+		n.writeSourceTo(b)
+	}
+
+	return nil
+}
+
+// writeIfDiff writes b to target but only if b represents a diff from the
+// current contents of target.
+func writeIfDiff(b *bytes.Buffer, target string, curVal []byte) error {
+	var err error
+	var exists bool
+	if curVal == nil {
+		// The file might not exist
+		curVal, err = os.ReadFile(target)
+		if err == nil {
+			exists = true
+		}
+	} else {
+		// assume the file exists
+		exists = true
+	}
+	// Early return if the target exists and is byte-equal
+	if exists && bytes.Equal(curVal, b.Bytes()) {
+		return nil
+	}
+	targetDir := filepath.Dir(target)
+	if err := os.MkdirAll(targetDir, 0777); err != nil {
+		return fmt.Errorf("failed to ensure %s exists: %w", targetDir, err)
+	}
+	if err := os.WriteFile(target, b.Bytes(), 0666); err != nil {
+		return fmt.Errorf("failed to write to %s: %w", target, err)
+	}
+	return nil
 }
---
+++ internal/cmd/preprocessor/cmd/sidebysidenode.go
@@ -0,0 +1,162 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"cuelang.org/go/cue/format"
+
+	encjson "encoding/json"
+
+	"cuelang.org/go/encoding/json"
+	"github.com/rogpeppe/go-internal/txtar"
+)
+
+const (
+	fnSidebyside = "sidebyside"
+
+	goldenExt = ".golden"
+)
+
+type sidebysideNode struct {
+	rf    *rootFile
+	lang  string
+	label string
+	ar    *txtar.Archive
+}
+
+var _ node = (*sidebysideNode)(nil)
+
+func (s *sidebysideNode) run() error {
+	// First format all non-output files
+	for i := range s.ar.Files {
+		f := &s.ar.Files[i]
+		a := analyseFilename(f.Name)
+		if a.isOut {
+			continue
+		}
+		switch a.ext {
+		case "json":
+			expr, err := json.Extract(f.Name, f.Data)
+			if err != nil {
+				return fmt.Errorf("failed to extract JSON from %s: %w", f.Name, err)
+			}
+			v := s.rf.page.ctx.executor.ctx.BuildExpr(expr)
+			if err := v.Err(); err != nil {
+				return fmt.Errorf("failed to build CUE value from %s: %w", f.Name, err)
+			}
+			byts, err := encjson.MarshalIndent(v, "", "  ")
+			if err != nil {
+				return fmt.Errorf("failed to format CUE value as json for %s: %w", f.Name, err)
+			}
+			f.Data = byts
+		case "yaml":
+			// TODO implement.
+		case "cue":
+			b, err := format.Source(f.Data)
+			if err != nil {
+				s.rf.page.debugf("failed to format CUE in %q %q: %w", s.label, f.Name, err)
+			} else {
+				f.Data = b
+			}
+		}
+	}
+	return nil
+}
+
+func (s *sidebysideNode) writeSourceTo(b *bytes.Buffer) {
+	p := bufPrintf(b)
+	p("```coq\n")
+	p("%swith %s %q %q%s\n", s.rf.page.leftDelim, fnSidebyside, s.lang, s.label, s.rf.page.rightDelim)
+	p("%s", txtar.Format(s.ar))
+	p("%send%s\n", s.rf.page.leftDelim, s.rf.page.rightDelim)
+	p("```\n")
+}
+
+func (s *sidebysideNode) writeTransformTo(b *bytes.Buffer) error {
+	p := bufPrintf(b)
+	var locations []string
+	switch l := len(s.ar.Files); l {
+	case 2:
+		// real side-by-side. Yes, you read the following line correctly
+		locations = []string{"top-left", "top-right"}
+	case 3:
+		locations = []string{"top-left", "bottom-left", "top-right"}
+	default:
+		var b bytes.Buffer
+		s.writeSourceTo(&b)
+		return fmt.Errorf("do not know how to handle %d txtar files: \n%s", l, b.Bytes())
+	}
+	p("{{< code-tabs >}}\n")
+	for i, f := range s.ar.Files {
+		a := analyseFilename(f.Name)
+		var typ string
+		name := f.Name
+		if a.isOut {
+			name = "TERMINAL"
+			typ = `type="terminal"`
+		}
+		p("{{< code-tab name=%q language=%q %v area=%q >}}\n", name, a.ext, typ, locations[i])
+		p("%s", f.Data)
+		p("{{< /code-tab >}}\n")
+	}
+	p("{{< /code-tabs >}}\n")
+	return nil
+}
+
+type filenameAnalysis struct {
+	// filepath is the original file path
+	filepath string
+
+	// basename is the basename of the last element of filepath
+	basename string
+
+	// ext is the bit of the last element of filepath after the last '.' (unlike
+	// filepath.Ext which starts at the final '.')
+	ext string
+
+	// isGolden is set if the file is considered a golden file
+	isGolden bool
+
+	// isOut is set if the file is considered an output in the archive
+	isOut bool
+}
+
+// analyseFilename provides useful details about a file in a txtar archive.
+func analyseFilename(p string) (res filenameAnalysis) {
+	res.filepath = p
+	b := filepath.Base(p)
+	b, res.isGolden = strings.CutSuffix(b, goldenExt)
+	i := len(b) - 1
+	for ; i >= 0 && !os.IsPathSeparator(b[i]); i-- {
+		if b[i] == '.' {
+			break
+		}
+	}
+	res.basename = b[:i]
+	if i < len(b) {
+		res.ext = b[i+1:]
+	}
+	switch res.basename {
+	case "out", "stdout":
+		res.isOut = true
+	}
+	return
+}
