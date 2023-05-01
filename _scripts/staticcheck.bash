74908f089ee9b18bf9ebbfbdd5334778fa38c225
Paul Jolly <paul@myitcv.io>
2023-05-01 16:43:13 +0100

    internal/parse: "vendor" text/template/parse
    This prepares for a later CL where we tweak the package to allow for
    custom delimiters on the String() output.
    
    The code is vendored as of f742ddc349723667fc9af5d0f16233f7762aeaa0.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I618cecae4c5771eee0eac1c5b00d9279ce5c2bae

--- _scripts/staticcheck.bash
+++ _scripts/staticcheck.bash
@@ -3,5 +3,6 @@
 set -eu
 
 go run honnef.co/go/tools/cmd/staticcheck@v0.4.3 $(go list ./... |
-	grep -v github.com/cue-lang/cuelang.org/playground/internal/cuelang_org_go_internal
+	grep -v github.com/cue-lang/cuelang.org/playground/internal/cuelang_org_go_internal |
+	grep -v github.com/cue-lang/cuelang.org/internal/parse
 )
