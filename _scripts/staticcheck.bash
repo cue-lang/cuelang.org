4b8c4c056b22e800b1dc52ebfdff3da7694baadf
Paul Jolly <paul@myitcv.io>
2023-05-03 18:13:39 +0100

    ci: add staticcheck to trybot run
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Idbdf160b495b6d47aede8f8531efc839a6cae609

---
+++ _scripts/staticcheck.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eu
+
+go run honnef.co/go/tools/cmd/staticcheck@v0.4.3 $(go list ./... |
+	grep -v github.com/cue-lang/cuelang.org/playground/internal/cuelang_org_go_internal
+)
