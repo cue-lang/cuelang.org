f3fdba77476453e783ab48ce5f9acd0b9a10bbc8
Anne van Gorkom <anne.van.gorkom@usmedia.nl>
2023-05-11 17:07:14 +0200

    Hugo: Fill and style search result page with algolia
    - Add Algolia Search package
    - Restyle search bar and page
    - Remove Google Search
    - Add Algolia logo (mandatory with free plan)
    - Add search widget for getting the results from Algolia
    - Add search teaser styling
    
    For feature/cue-210-search-redo-search-page-replace-google
    
    Closes #348 as merged as of commit 92efec03.
    
    Signed-off-by: Anne van Gorkom <anne.van.gorkom@usmedia.nl>
    Change-Id: I7ede6947e6beead7498b1adc39ac610a9585217c
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/554289
    Reviewed-by: Chief Cueckoo <chief.cueckoo@gmail.com>
    TryBot-Result: CUEcueckoo <cueckoo+gerrithub@cuelang.org>

---
+++ hugo/assets/ts/interfaces/search.ts
@@ -0,0 +1,9 @@
+export interface SearchItem {
+    objectID: string;
+    title: string;
+    link: string;
+    categories: string | string[];
+    summary: string;
+    publishDate: string;
+    content: string;
+}
