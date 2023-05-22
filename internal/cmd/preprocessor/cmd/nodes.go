cbdebf4ebf7561025913aabad84d1ab3c5130a26
Paul Jolly <paul@myitcv.io>
2023-05-22 17:47:37 +0100

    preprocessor: validate that we don't use a label multiple times
    Per page per node type, we ensure that we don't re-use labels.
    Otherwise it's impossible to refer to the structure of a script in a
    meaningful way, especially in error messages.
    
    As part of this same change, introduce a Type() method on a node, so
    that we can print a human-friendly name as part of error messages.
    
    Also introduce a labelledNode interface which is used to identify those
    nodes which label themselves as a means of identification. This allows
    for referencing of these nodes in error messages, but also in comparison
    with a structured (CUE) form on a page.
    
    Introduce a test to ensure we correctly report errors where the same
    label is used multiple times for a sidebyside node. But also fix up
    inadvertent errors in an existing test.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I4febca23ca2616e165cf326ec16459331fc5597b

--- internal/cmd/preprocessor/cmd/nodes.go
+++ internal/cmd/preprocessor/cmd/nodes.go
@@ -32,10 +32,14 @@ type node interface {
 	// writeTransformTo is the transform or output step of the preprocessor.
 	writeTransformTo(buf *bytes.Buffer) error
 
+	Type() string
+
 	fmt.Formatter
 }
 
 type runnableNode interface {
+	node
+
 	// run is called to cause a node to update itself. The simplest example is a
 	// node that includes a testscript script. Running such a node would involve
 	// running the script, ensuring it passes and is therefore consistent with
@@ -43,6 +47,13 @@ type runnableNode interface {
 	// indicate that scripts should be updated to ensure assertions pass (e.g.
 	// testscript.Params.UpdateScripts)
 	run() runnable
+}
+
+type labelledNode interface {
+	node
+
+	// A Label uniquely identifies a nodes of each types.
+	Label() string
 }
 
 type runnable interface {
@@ -80,6 +91,10 @@ func (n *nodeWrapper) writeTransformTo(b *bytes.Buffer) error {
 
 func (n *nodeWrapper) Format(f fmt.State, verb rune) {
 	fmt.Fprintf(f, "%v:%s", n.rf, n.rf.nodePos(n.underlying))
+}
+
+func (n *nodeWrapper) Type() string {
+	return fmt.Sprintf("nodeWrapper(%T)", n.underlying)
 }
 
 type textNode struct {
