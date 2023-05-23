b939b293d20e64c6d6ffaad904bee8896e515b3f
Paul Jolly <paul@myitcv.io>
2023-05-23 06:03:36 +0100

    preprocessor: rework error handling (again)
    Now that we've had a chance to see how things "look" with the previous
    cut of error handling in place, rework based on rough edges.
    
    Changes:
    
    * Rename errorContextInterface -> errorContext and create a new
      bufferedErrorContext interface for the buffered version. These are
      more sensible names.
    * Name errorContextWriter and errorContextBuffer as the "best" names for
      the single implementations of the aforementioned interfaces.
    * Do not attempt reuse via embedding between errorContextWriter and
      errorContextBuffer. Reuse for the purposes of avoiding writing methods
      twice simply creates problems where either type is used and embedded
      alongside other types (e.g. nodeWrapper) that themselves embed one or
      the other (because we then end up with ambiguous method references).
    * For types that embed either errorContextWriter or errorContextBuffer,
      embed the interface type and not the implementation. This makes the
      reuse of either more natural.
    * Separate out the embedding of *executionContext. Whilst this is
      required by both errorContextWriter and errorContextBuffer, the
      embedding of either of values of either type should not be the means
      by which we reference fields on executionContext.
    * Therefore, consistently embed the "pair" of *executionContext and
      (errorContext|bufferedErrorContext), and ensure that both are set in
      constructors/equivalent.
    * Move the *cue.Context field to executionContext, where it belongs.
      This should be readily available to any method during the course of an
      execute command.
    * Various documentation improvements for types.
    * Delete (*sidebysideNode).Format because sidebysideNode now embeds
      *nodeWrapper (which is possible because we've removed ambiguities in
      methods referenced via embeddings).
    * Simplify references to fields by using embeddings where possible. This
      made possible by dropping attempted reuse between the old
      bufferedErrorContext and bufferedErrorContext.
    * Move bufPrintf to sidebysidenode.go where it is used.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I3d737ceebb312bf37158fd26b3cd4c29beb9c68e

--- internal/cmd/preprocessor/cmd/bufferederrorcontext.go
+++ internal/cmd/preprocessor/cmd/bufferederrorcontext.go
@@ -16,27 +16,75 @@ package cmd
 
 import (
 	"bytes"
-	"io"
+	"fmt"
+
+	"cuelang.org/go/cue/errors"
 )
 
-type bufferedErrorContext struct {
+// bufferedErrorContext is a buffered extension of errorContext. That is, all
+// log messages are buffered, and then later retrieved via the bytes() method.
+type bufferedErrorContext interface {
 	errorContext
 
-	// log is the buffer to which we write log messages
-	log bytes.Buffer
+	// bytes returns the log messages buffered.
+	bytes() []byte
 }
 
-func newBufferedErrorContext(e errorContextInterface) *bufferedErrorContext {
-	res := new(bufferedErrorContext)
-	res.errorContext.log = &res.log
-	res.executionContext = e.execContext()
+// errorContextBuffer is a base implementation of bufferedErrorContext
+//
+// Is is intentionally not threadsafe.
+type errorContextBuffer struct {
+	*executionContext
+	inError bool
+	log     bytes.Buffer
+}
+
+var _ errorContext = (*errorContextBuffer)(nil)
+
+func (e *errorContextBuffer) isInError() bool {
+	return e.inError
+}
+
+func (e *errorContextBuffer) setInError(v bool) {
+	e.inError = e.inError || v
+}
+
+func (e *errorContextBuffer) errorIfInError() error {
+	if !e.inError {
+		return nil
+	}
+	return errors.New("in error")
+}
+
+func (e *errorContextBuffer) logf(format string, args ...any) string {
+	res := fmt.Sprintf(format, args...)
+	m := res
+	if len(m) > 0 && m[len(m)-1] != '\n' {
+		m += "\n"
+	}
+	fmt.Fprint(&e.log, m)
 	return res
 }
 
-func (e *bufferedErrorContext) logger() io.Writer {
-	return &e.log
+func (e *errorContextBuffer) errorf(format string, args ...any) error {
+	s := fmt.Sprintf(format, args...)
+	e.inError = true
+	return errors.New(e.logf("** %s", s))
 }
 
-func (e *bufferedErrorContext) bytes() []byte {
+func (e *errorContextBuffer) fatalf(format string, args ...any) {
+	err := e.errorf(format, args...)
+	panic(fatalError{error: err})
+}
+
+func (e *errorContextBuffer) debugf(format string, args ...any) {
+	if !e.debug {
+		return
+	}
+	s := fmt.Sprintf(format, args...)
+	e.logf("debug: " + s)
+}
+
+func (e *errorContextBuffer) bytes() []byte {
 	return e.log.Bytes()
 }
