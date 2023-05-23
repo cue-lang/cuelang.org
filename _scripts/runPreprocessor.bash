c5d574060ed251bff7d15ef6a9e6e95919cc3cd4
Paul Jolly <paul@myitcv.io>
2023-05-23 16:34:08 +0100

    preprocessor: create a script to run preprocessor
    This ensures that everyone (who reads the docs) runs the preprocessor in
    the same way.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: If7286fe1ae31a857971700d1d8fcf727d7ba2b85
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/554317
    TryBot-Result: CUEcueckoo <cueckoo+gerrithub@cuelang.org>
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>

---
+++ _scripts/runPreprocessor.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# cd to the parent directory to that containing the script
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+go run -trimpath ./internal/cmd/preprocessor "$@"
