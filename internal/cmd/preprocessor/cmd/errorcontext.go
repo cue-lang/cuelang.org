0e492cad3d584f23ffff5b76ee4de06c708a41eb
Paul Jolly <paul@myitcv.io>
2023-05-26 17:28:19 +0100

    preprocessor: fix bug with logging of empty log message
    This bug got introduced between when addressing feedback in
    https://cuelang.org/cl/554281. It was only spotted in failing tests in
    later CLs in the chain.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I1cdd03dd9e15cae9a86f7f31e31084360104c3f5
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/554656
    Reviewed-by: Daniel Martí <mvdan@mvdan.cc>
    TryBot-Result: CUEcueckoo <cueckoo+gerrithub@cuelang.org>

--- internal/cmd/preprocessor/cmd/errorcontext.go
+++ internal/cmd/preprocessor/cmd/errorcontext.go
@@ -65,7 +65,7 @@ func (e *errorContextWriter) logf(format string, args ...any) string {
 	var res bytes.Buffer
 	w := io.MultiWriter(&res, e.log)
 	fmt.Fprintf(w, format, args...)
-	if !bytes.HasSuffix(res.Bytes(), []byte("\n")) {
+	if b := res.Bytes(); len(b) > 0 && !bytes.HasSuffix(b, []byte("\n")) {
 		fmt.Fprint(w, "\n")
 	}
 	return res.String()
