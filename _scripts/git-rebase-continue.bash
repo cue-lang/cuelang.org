64fe1faaaae20bcdd3e5fa49b2a2c1727fd1c0dc
Paul Jolly <paul@myitcv.io>
2023-05-29 09:28:29 +0100

    _scripts: add scripts to run as part of git commands
    git does not fire any hooks when "creating" commits as part of a rebase.
    This makes it impossible to run commands that ensure we have the current
    results of code generation as part of that commit. Instead, we can use
    the -x flag to git rebase to run a commit after each entry in an
    interactive rebase. If the command produces output that is not present
    in the then previous commit, it can be written back via git commit
    --amend. We encapsulate the steps that should be run in this "post
    rebase commit" phase in _scripts/git-rebase-x.bash. The script is used
    as follows:
    
      git rebase -i -x ./_scripts/git-rebase-x.bash $commit
    
    where $commit is generally the merge base of the current branch with the
    upstream.
    
    Another source of challenges is when conflicts occur as a result of
    regenerating files in earlier commits. If you are making changes to the
    preprocessor, this should happen on each commit. Again, git does not
    have great features for helping here. The closest we could come is using
    a custom merge driver that wraps git merge-org, but even that is not
    possible as of today. So for now we write a script that can easily (be
    aliased and) be run when conflicts do occur: we encapsulate that logic
    in _scripts/git-rebase-continue.bash.
    
    Both changes make interactive rebasing a more pleasant experience, by
    automatically re-running the go generate direct that contains the
    genpkghash command, but also re-running the preprocessor. This saves us
    having to remember to run either of these after each commit. The cost of
    doing so is amortised by the code generator being fast, and the
    preprocessor heavily using caching.  Where conflicts do occur, a simple
    script can be run to recover.
    
    In future it might actually be simpler to wrap git rebase and intercept
    git rebase --continue with a retry loop, but for now we keep things
    simple via a "recover" script.
    
    Note the git-rebase-continue.bash script is conservative in what it
    does. This means that if there are conflicts in other non-generated
    files the rebase will still stop.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I694382719ded08f4116ecc43b6cf97ca526f476a

---
+++ _scripts/git-rebase-continue.bash
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+# cd to the parent directory to that containing the script
+command cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+set -eu
+
+git checkout --theirs content/**/gen_cache.cue
+go generate -x ./internal/cmd/preprocessor/cmd
+./_scripts/runPreprocessor.bash execute
+git add content/**/gen_cache.cue
+GIT_EDITOR=true git rebase --continue
