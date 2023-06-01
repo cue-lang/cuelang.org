da9e208f8d6149f495082628dfaa01b80f1e3e95
Paul Jolly <paul@myitcv.io>
2023-06-01 17:17:07 +0100

    preprocessor: interrupt hugo when preprocessor is interrupted
    This ensures we properly tidy up, without relying on the shell (or
    something else) killing the process group to which the preprocessor
    belongs.
    
    Remove any potential script-related signal issues by exec-ing processes
    at the end of each script.
    
    Also write a test for preprocessor execute --serve, specifically a test
    that calls the script that we tell contributors to run when developing
    locally. This will help us to stop breaking developers inadvertently.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Idadd77d6df487dbbb8f7ba1dac02b5038ecbaa00

---
+++ internal/cmd/preprocessor/main_test.go
@@ -0,0 +1,95 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// Ensure that we can run the default serve script to render the homepage
+func TestServeScript(t *testing.T) {
+	ctx, done := context.WithCancel(context.Background())
+	wg, ctx := errgroup.WithContext(ctx)
+
+	var buf bytes.Buffer
+
+	cwd, _ := os.Getwd()
+	cmd := exec.CommandContext(ctx, "./_scripts/serve.bash")
+	cmd.Cancel = func() error {
+		return cmd.Process.Signal(os.Interrupt)
+	}
+	cmd.Stdout = &buf
+	cmd.Stderr = &buf
+	cmd.Dir = filepath.Join(cwd, "..", "..", "..")
+
+	// Run serve
+	wg.Go(func() error {
+		if err := cmd.Start(); err != nil {
+			return fmt.Errorf("serve failed to start: %v", err)
+		}
+		if err := cmd.Wait(); err != nil && !errors.Is(err, context.Canceled) {
+			return fmt.Errorf("serve failed to wait for command: %v", err)
+		}
+		return nil
+	})
+
+	// Run check
+	wg.Go(func() error {
+		var i int
+		for {
+			select {
+			case <-ctx.Done():
+				break
+			default:
+			}
+			i++
+			if i == 10 {
+				return fmt.Errorf("timed out trying to fetch")
+			}
+			time.Sleep(time.Second)
+			resp, err := http.Get("http://localhost:1313/docs/")
+			if err == nil {
+				// server is up and running
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					return fmt.Errorf("failed to read response body: %v", err)
+				}
+				if !bytes.Contains(body, []byte("Welcome to CUE")) {
+					return fmt.Errorf("body did not contain expected value:\n%s", body)
+				}
+				break
+			}
+		}
+		done()
+		return nil
+	})
+
+	if err := wg.Wait(); err != nil {
+		t.Fatalf("failed: %v\n%s", err, buf.Bytes())
+	}
+}
