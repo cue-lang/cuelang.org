3129f7a8b34f49bbc9820cbb199f1d25033473e9
Paul Jolly <paul@myitcv.io>
2023-08-09 16:55:30 +0100

    preprocessor: add --noWriteCache flag
    TODO: write this commit message
    
    PREPROCESSOR-NO-WRITE-CACHE
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I44a5e2543b578ca8538bda85535b997f15eb376a

--- _scripts/runPreprocessor.bash
+++ _scripts/runPreprocessor.bash
@@ -5,5 +5,14 @@ set -eux
 # cd to the parent directory to that containing the script
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
+# In CI, we set the PREPROCESSOR_NOWRITECACHE env var only under certain
+# conditions. See internal/ci/github/trybot.cue for more details. When running
+# locally, we set the env var based on whether the HEAD commit contains
+# PREPROCESSOR-NO-WRITE-CACHE on a clear line.
+if [ "${CI:-}" != "true" ] && git log --format=%b -1 HEAD | grep -q PREPROCESSOR-NO-WRITE-CACHE
+then
+	export PREPROCESSOR_NOWRITECACHE="true"
+fi
+
 GOBIN=$PWD/.gobin go install -trimpath -buildvcs=false ./internal/cmd/preprocessor
 exec $PWD/.gobin/preprocessor "$@"
