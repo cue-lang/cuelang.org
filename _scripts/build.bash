e539a2b58fc1c1a119b8e7d36d2d6490838f20cf
Jonathan Matthews <github@hello.jonathanmatthews.com>
2023-09-05 12:12:10 +0100

    ci: show envvar values available inside netlify
    This displays a set of envvar values during builds, as defined at
    https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables.
    
    This is to let us choose the variable(s) we need to pass through to the
    hugo build process as our baseURL ("-b") parameter, across our different
    preview/branch/prod/etc builds.
    
    Change-Id: Ib49f571079699c9266bb08bc96c401ffaf18222d
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

--- _scripts/build.bash
+++ _scripts/build.bash
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+## Temporary exploration of the Netlify build context.
+### This is safe to remove at any time.
+set +u
+if [[ "$NETLIFY" = "true" ]]; then
+    printf 'env: CONTEXT: "%s"\n'          "$CONTEXT" >&2
+    printf 'env: BRANCH: "%s"\n'           "$BRANCH" >&2
+    printf 'env: HEAD: "%s"\n'             "$HEAD" >&2
+    printf 'env: URL: "%s"\n'              "$URL" >&2
+    printf 'env: DEPLOY_URL: "%s"\n'       "$DEPLOY_URL" >&2
+    printf 'env: DEPLOY_PRIME_URL: "%s"\n' "$DEPLOY_PRIME_URL" >&2
+    printf 'env: SITE_NAME: "%s"\n'        "$SITE_NAME" >&2
+fi
+## End of build context exploration.
+
 set -eux
 
 # build.bash is used to build the site ready for deploy.
