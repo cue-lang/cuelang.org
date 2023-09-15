2cfe691f812cf2337c2ec0b918ee9e21a0f55b9f
Paul Jolly <paul@myitcv.io>
2023-09-15 15:36:07 +0100

    preprocessor: first cut of evaluation of multi-step scripts
    A first cut implementation of the execution of multi-step scripts.
    
    This code largely borrows from previous work in preguide:
    
        https://github.com/play-with-go/preguide
    
    with permission from the author, @myitcv.
    
    As part of the changes we add a dependency on mvdan.cc/sh/v3, a
    well-established and mature shell parsing library. Adding the dependency
    has the effect of bumping a few shared dependencies.
    
    Theoretically a guide can contain self-contained runnable nodes
    (currently only the sidebyside directive) and a multi-step script (a
    sequence of upload and script directives). In reality, we generally see
    one or the other.
    
    The logic for running the sidebyside (self-contained) directives is
    unchanged in this CL. Each represents a unit of runnable work.
    
    In a similar way, the multi-step script implied a sequence of upload and
    script directives.
    
    The validation of a script node now includes an attempt to parse the
    contents of the script node as a bash script. We translate the parsed
    result into a commandStmt value, which holds state/fields necessary for
    writing the transformed output of a script node. See the type doc
    comment for more information.
    
    We build a multi-step script from the upload and script directives in a
    guide, and add the running of that bash script to a queue of runnable
    work for that guide. Given that pages are processed concurrently, we end
    up running runnable tasks from multiple pages (including the runnable
    work that is a multi-step script) at the same time. Again, there is no
    change in this logic, repeating for context.
    
    The bash script generated from the upload and script directives is
    composed in (*rootFile).buildMultistepScript. It uses a similar
    technique to preguide writing delimiters to make the parsing of the
    output with respect to individual statements trivial.
    
    The running of a multi-step script again uses Docker for safety reasons.
    A script node could in theory do anything at all, including removing
    files from the root of the file system.
    
    The running in docker uses "docker create" and "docker start -a" to
    avoid issues with stdin+stdout+stderr not being a TTY. Whilst I don't
    recall the exact reasons, this lesson was hard-learnt with preguide.
    
    The script node transformation step is updated to include the
    calculation of the base64 encoding of the non-adulterated script input,
    i.e. no '$' signs and no output.
    
    The execute_multistagescript test is updated to wrap the upload and
    script steps in a step directive to ensure we correctly process "nested"
    directives. This could have been made in another change, but it doesn't
    seem bad to include here. The expected output of this test is updated
    now that we can actually running the script steps.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Preprocessor-No-Write-Cache: true
    Change-Id: Ia7224f135dea975badc63c7a249dc545d8f772af

--- internal/cmd/preprocessor/cmd/script_node.go
+++ internal/cmd/preprocessor/cmd/script_node.go
@@ -16,34 +16,92 @@ package cmd
 
 import (
 	"bytes"
+	"encoding/base64"
 	"fmt"
+	"strings"
+
+	"mvdan.cc/sh/v3/syntax"
 )
 
 const fnScript = "script"
 
 type scriptNode struct {
 	txtarNode
+
+	stmts []*commandStmt
 }
 
 func (s *scriptNode) nodeType() string {
 	return "script"
 }
 
+// validate the scriptNode. This also has the side effect of parsing the
+// bash script in the comment o commandStmt's.
 func (s *scriptNode) validate() error {
 	if l := len(s.analysis.fileNames); l != 0 {
 		return fmt.Errorf("script nodes cannot contain any files in the txtar archive")
 	}
 
+	// Now ensure that we can parse the script
+	//
+	// Per @mvdan, use https://pkg.go.dev/mvdan.cc/sh/v3/syntax#Parser.Stmts
+	file, err := syntax.NewParser(syntax.KeepComments(true)).Parse(bytes.NewReader(s.effectiveArchive.Comment), "")
+	if err != nil {
+		return fmt.Errorf("failed to parse shell script: %v", err)
+	}
+	s.debugf(s.debugGeneral, "parsed %q, gave %v statements", s.effectiveArchive.Comment, len(file.Stmts))
+	for _, stmt := range file.Stmts {
+		var cmdStmt commandStmt
+		// Capture whether this statement is negated or not
+		negated := stmt.Negated
+		// Set to not negated because we need to capture the exit code.
+		// Handling of the exit code and negated happens in the generated
+		// bash script
+		stmt.Negated = false
+		var sb strings.Builder
+		if err := s.rf.shellPrinter.Print(&sb, stmt); err != nil {
+			return fmt.Errorf("failed to print statement: %v", err)
+		}
+		cmdStmt.cmdStr = sb.String()
+		cmdStmt.negated = negated
+		s.stmts = append(s.stmts, &cmdStmt)
+	}
+
 	return nil
+}
+
+// commandStmt is effectively a local version of the parsed *syntax.Stmt. This
+// struct holds the details we need to build bash script to run in docker, but
+// also to capture a specific statement's output ready for rendering. See
+// (*scriptNode).validate() for more information.
+type commandStmt struct {
+	negated     bool
+	cmdStr      string
+	exitCode    int
+	output      string
+	outputFence string
 }
 
 func (s *scriptNode) writeTransformTo(b *bytes.Buffer) error {
 	p := bufPrintf(b)
 	// With a script there are no files... just script
-	script := s.effectiveArchive.Comment
-	p("```text { title=%q", "TERMINAL")
+
+	// Build up a base64 encoded script that will be copied. This is necessary
+	// because the rendered window will include the output... which won't work
+	// if copy-pasted.
+	var copyCmdStr strings.Builder
+	enc := base64.NewEncoder(base64.StdEncoding, &copyCmdStr)
+	for _, stmt := range s.stmts {
+		fmt.Fprintf(enc, "%s\n", stmt.cmdStr)
+	}
+	enc.Close()
+
+	p("```text { title=%q codeToCopy=%q", "TERMINAL", copyCmdStr.String())
 	p(" }\n")
-	p("%s", script)
+	for _, stmt := range s.stmts {
+		p("$ %s\n", stmt.cmdStr)
+		p("%s", stmt.output)
+	}
 	p("```")
 	return nil
 }
