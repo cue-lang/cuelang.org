9616e05e5a24612cea16d3db3d00bcaad057a951
Paul Jolly <paul@myitcv.io>
2023-09-17 05:33:12 +0100

    preprocessor: lift code-tabs code out of sidebyside node
    This will be used for upload and script tabs in later CLs.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ib9bf1b91749a19fd20e0337b306d0de79677a4b0
    Preprocessor-No-Write-Cache: true

---
+++ internal/cmd/preprocessor/cmd/hugo.go
@@ -0,0 +1,23 @@
+package cmd
+
+// tabProps captures the inputs required to a code-tabs hugo output
+type tabProps struct {
+	Name     string
+	Language string
+	Type     string
+}
+
+type codeTabLocation string
+
+const (
+	//lint:ignore U1000 we will use these later, documenting for completeness' sake now
+	codeTabTop    codeTabLocation = "top"
+	codeTabBottom codeTabLocation = "bottom"
+	codeTabLeft   codeTabLocation = "left"
+	codeTabRight  codeTabLocation = "right"
+
+	codeTabTopLeft     codeTabLocation = "top-left"
+	codeTabTopRight    codeTabLocation = "top-right"
+	codeTabBottomLeft  codeTabLocation = "bottom-left"
+	codeTabBottomright codeTabLocation = "bottom-right"
+)
