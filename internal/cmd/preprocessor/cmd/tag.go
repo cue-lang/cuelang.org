aba78408cbd151685433b61ea0adc91958d93124
Paul Jolly <paul@myitcv.io>
2023-09-17 20:30:30 +0100

    preprocessor: separate out tags into separate file
    At the same time, document the existing norun tag.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I5e75b1ec84c7c5d15db29fbe81876a419fffa0f5
    Preprocessor-No-Write-Cache: true

---
+++ internal/cmd/preprocessor/cmd/tag.go
@@ -0,0 +1,25 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+const (
+	// tagNorun is the tag used in a txtar-based directive like sidebyside or script
+	// to indicate that that node should not be run. For an upload directive, it
+	// means the commands in the step are not included in the multi-step script that
+	// is compiled, but the commands are included in the output. Thus the rendered
+	// output is just the commands, but not any output they would otherwise have
+	// generated.
+	tagNorun = "norun"
+)
