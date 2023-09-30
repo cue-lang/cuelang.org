90852ee03bcfea9fc312f7a53b5d45d4688a6243
Paul Jolly <paul@myitcv.io>
2023-09-30 05:12:15 +0100

    internal/ci: use writefs
    As a further experiment, switch to using writefs, which is copied by
    hand into this repo as an internal command.
    
    There is no speed advantage to using it, rather it is a usability and
    readability win.
    
    Kicking the tyres with it more in this repo will help to see whether the
    pattern works or not.
    
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Ia9203e2138ad74947ba49614df3298d84b7aa860

--- internal/ci/ci_tool.cue
+++ internal/ci/ci_tool.cue
@@ -1,4 +1,4 @@
-// Copyright 2021 The CUE Authors
+// Copyright 2023 The CUE Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,69 +15,11 @@
 package ci
 
 import (
-	"path"
-	"encoding/yaml"
-	"tool/file"
-
-	"github.com/cue-lang/cuelang.org/internal/ci/base"
-	"github.com/cue-lang/cuelang.org/internal/ci/repo"
-	"github.com/cue-lang/cuelang.org/internal/ci/github"
-	_netlify "github.com/cue-lang/cuelang.org/internal/ci/netlify"
+	"encoding/json"
+	"tool/exec"
 )
 
-// For the commands below, note we use simple yet hacky path resolution, rather
-// than anything that might derive the module root using go list or similar, in
-// order that we have zero dependencies.  This is important because this CUE
-// package is "vendored" to an external dependency so that that unity can
-// re-run and verify these steps as part of a the test suite that runs against
-// new CUE versions.
-
-_goos: string @tag(os,var=os)
-
-// gen.workflows regenerates the GitHub workflow Yaml definitions.
-//
-// See internal/ci/gen.go for details on how this step fits into the sequence
-// of generating our CI workflow definitions, and updating various txtar tests
-// with files from that process.
-command: gen: {
-	_dir: path.FromSlash("../../.github/workflows", path.Unix)
-
-	workflows: {
-		remove: {
-			glob: file.Glob & {
-				glob: path.Join([_dir, "*.yml"], _goos)
-				files: [...string]
-			}
-			for _, _filename in glob.files {
-				"delete \(_filename)": file.RemoveAll & {
-					path: _filename
-				}
-			}
-		}
-		for _workflowName, _workflow in github.workflows {
-			let _filename = _workflowName + ".yml"
-			"generate \(_filename)": file.Create & {
-				$after: [ for v in remove {v}]
-				filename: path.Join([_dir, _filename], _goos)
-				let donotedit = base.doNotEditMessage & {#generatedBy: "internal/ci/ci_tool.cue", _}
-				contents: "# \(donotedit)\n\n\(yaml.Marshal(_workflow))"
-			}
-		}
-	}
-}
-
-command: gen: netlify: file.Create & {
-	_dir:     path.FromSlash("../../", path.Unix)
-	filename: path.Join([_dir, "netlify.toml"], _goos)
-	let res = _netlify.#toToml & {#input: _netlify.config, _}
-	let donotedit = base.doNotEditMessage & {#generatedBy: "internal/ci/ci_tool.cue", _}
-	contents: "# \(donotedit)\n\n\(res)\n"
-}
-
-command: gen: codereviewcfg: file.Create & {
-	_dir:     path.FromSlash("../../", path.Unix)
-	filename: path.Join([_dir, "codereview.cfg"], _goos)
-	let res = base.toCodeReviewCfg & {#input: repo.codeReview, _}
-	let donotedit = base.doNotEditMessage & {#generatedBy: "internal/ci/ci_tool.cue", _}
-	contents: "# \(donotedit)\n\n\(res)\n"
+command: gen: exec.Run & {
+	cmd: ["go", "run", "github.com/cue-lang/cuelang.org/internal/cmd/writefs"]
+	stdin: json.Marshal(fs)
 }
---
+++ internal/cmd/writefs/main.go
@@ -0,0 +1,103 @@
+// Copyright 2023 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+type file struct {
+	Type string
+
+	// If typ == "symlink" this is the filepath of the target
+	// otherwise it is the Contents of the file
+	Contents string
+}
+
+type args struct {
+	// Remove is a list of globs of files to remove. File
+	// removal happens before file creation.
+	Remove []string
+
+	// Create is a map of file path (in Unix format) to contents.
+	Create map[string]file
+}
+
+func main() {
+	var todo args
+	dec := json.NewDecoder(os.Stdin)
+	if err := dec.Decode(&todo); err != nil {
+		log.Fatalf("failed to decode arguments from stdin: %v", err)
+	}
+	for _, glob := range todo.Remove {
+		files, err := filepath.Glob(glob)
+		if err != nil {
+			log.Fatalf("failed to glob %q: %v", glob, err)
+		}
+		if len(files) == 0 {
+			continue
+		}
+		for _, f := range files {
+			if err := os.Remove(f); err != nil {
+				log.Fatalf("failed to remove %s: %v", f, err)
+			}
+		}
+	}
+	for fp, f := range todo.Create {
+		fp = filepath.FromSlash(fp)
+		dir := filepath.Dir(fp)
+		if err := os.MkdirAll(dir, 0o777); err != nil {
+			log.Fatalf("failed to mkdir %s: %v", dir, err)
+		}
+		switch f.Type {
+		case "symlink":
+			target := filepath.FromSlash(f.Contents)
+			actualTarget, err := os.Readlink(fp)
+			if err == nil && actualTarget == target {
+				continue
+			}
+			if err := os.Symlink(target, fp); err != nil {
+				log.Fatalf("failed to symlink %s -> %s: %v", fp, target, err)
+			}
+		case "file":
+			// If we have a .json file, normalise the contents first
+			// as a special case.
+			//
+			// TODO: this actually belongs as an option in CUE.
+			contents := []byte(f.Contents)
+			if filepath.Ext(fp) == ".json" {
+				var i any
+				err := json.Unmarshal(contents, &i)
+				if err == nil {
+					var b bytes.Buffer
+					enc := json.NewEncoder(&b)
+					enc.SetEscapeHTML(false)
+					enc.SetIndent("", "    ")
+					if err := enc.Encode(i); err == nil {
+						// Add a trailing newline
+						contents = b.Bytes()
+					}
+				}
+			}
+			if err := os.WriteFile(fp, []byte(contents), 0o666); err != nil {
+				log.Fatalf("failed to write file %s: %v", fp, err)
+			}
+		}
+	}
+}
