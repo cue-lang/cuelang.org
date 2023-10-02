c87531666ce98ec86a1aae785f41241995a6f010
Paul Jolly <paul@myitcv.io>
2023-10-02 17:16:01 +0100

    preprocessor: add _scripts/siteCUE.bash as cmd/cue wrapper
    Because we have a custom load mode of CUE files for the site
    configuration, it's helpful to have a command to behave like cmd/cue but
    that loads the relevant files.
    
    _scripts/siteCUE.bash is that wrapper, and it leverages `execute --ls`
    for its list of CUE inputs.
    
    We also have to do a bit of a dance with a temporary file in order that
    `cue cmd` works, via a gitignore-d file in the repo root.
    
    This script is used in later CLs.
    
    Preprocessor-No-Write-Cache: true
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I23b45a2d9f0d1d83911349df6261e349cd66d8af
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/1170196
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

---
+++ _scripts/siteCUE.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# siteCUE.bash is a simple wrapper around runPreprocessor.bash that
+# behaves like cmd/cue.
+#
+#     ./_scripts/siteCUE.bash export -e versions
+#
+# The one important detail is that if we are running cue cmd (i.e $1 == "cmd")
+# then we write a temporary file in the root of the repo so that the _tool.cue
+# files can see the result of the evaluation of the entire site, not just what
+# is at the root.  This temporary file is removed in a trap in this script.
+
+# cd to the parent directory to that containing the script
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+files=$( ./_scripts/runPreprocessor.bash execute --ls)
+
+# Unconditionally remove this file if it exists to start with
+# as it will interfere with evaluation, parsing etc.
+rm -f defresult.cue
+
+if [[ "$1" == "cmd" ]]; then
+	trap "rm -f defresult.cue" EXIT
+	cue def $files > defresult.cue
+fi
+
+go run cuelang.org/go/cmd/cue "$@" $files
