ba1dcea6c66c9f842ba57536218b16c093277f83
Daniel Martí <mvdan@mvdan.cc>
2023-10-16 16:07:55 +0100

    playground: add test cases to show an export bug
    In particular, exporting this input as CUE works correctly,
    but exporting as JSON or YAML fails with an error.
    
    For #2417.
    
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: I20b3615b5e80d2bc27374661c143f0ce80228057

--- playground/impl_test.go
+++ playground/impl_test.go
@@ -30,26 +30,47 @@ var testTable = []struct {
 	{inputCUE, functionExport, outputCUE, "", "\n", ""},
 	{inputCUE, functionExport, outputCUE, "a: b: 5\na: c: 4", "a: {\n\tb: 5\n\tc: 4\n}\n", ""},
 	{inputCUE, functionExport, outputJSON, "test: 5", "{\n    \"test\": 5\n}\n", ""},
+
+	{
+		inputCUE, functionExport, outputCUE,
+		`
+			[ID=_]: x: y: ID
+			"foo": {}
+		`,
+		"foo: {\n\tx: {\n\t\ty: \"foo\"\n\t}\n}\n",
+		"",
+	},
+	{
+		inputCUE, functionExport, outputJSON,
+		`
+			[ID=_]: x: y: ID
+			"foo": {}
+		`,
+		"",
+		"failed to encode: foo.x.y: incomplete value string",
+	},
 }
 
 func TestHandleCUECompile(t *testing.T) {
 	for _, tv := range testTable {
-		desc := fmt.Sprintf("handleCUECompile(%q, %q, %q, %q)", tv.In, tv.Fn, tv.Out, tv.InVal)
-		out, err := handleCUECompile(tv.In, tv.Fn, tv.Out, tv.InVal)
-		if tv.Err != "" {
-			if err != nil {
-				if err.Error() != tv.Err {
-					t.Fatalf("%v: expected error string %q; got %q", desc, tv.Err, err)
+		t.Run("", func(t *testing.T) {
+			desc := fmt.Sprintf("handleCUECompile(%q, %q, %q, %q)", tv.In, tv.Fn, tv.Out, tv.InVal)
+			out, err := handleCUECompile(tv.In, tv.Fn, tv.Out, tv.InVal)
+			if tv.Err != "" {
+				if err != nil {
+					if err.Error() != tv.Err {
+						t.Fatalf("%v: expected error string %q; got %q", desc, tv.Err, err)
+					}
+				} else {
+					t.Fatalf("%v: expected error, did not see one. Output was %q", desc, out)
 				}
 			} else {
-				t.Fatalf("%v: expected error, did not see one. Output was %q", desc, out)
+				if err != nil {
+					t.Fatalf("%v: got unexpected error: %v", desc, err)
+				} else if out != tv.OutVal {
+					t.Fatalf("%v: expected output %q: got %q", desc, tv.OutVal, out)
+				}
 			}
-		} else {
-			if err != nil {
-				t.Fatalf("%v: got unexpected error: %v", desc, err)
-			} else if out != tv.OutVal {
-				t.Fatalf("%v: expected output %q: got %q", desc, tv.OutVal, out)
-			}
-		}
+		})
 	}
 }
