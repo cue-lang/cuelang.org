cc4402c4342c8ac2b51874946e2817fd7ba9db5d
Daniel Martí <mvdan@mvdan.cc>
2023-10-19 16:49:58 +0100

    playground: hook up the new eval function
    Signed-off-by: Daniel Martí <mvdan@mvdan.cc>
    Change-Id: Ic4814c5b8569dccab3539d981c36f2951012050a

--- playground/src/config/workspaces/function.ts
+++ playground/src/config/workspaces/function.ts
@@ -1,19 +1,19 @@
 import { Workspace, WORKSPACE } from '@models/workspace';
-import { OPTION_TYPE, optionCUE, optionDef, optionExport, optionJSON, optionYAML } from '@models/options';
+import { OPTION_TYPE, optionCUE, optionDef, optionEval, optionExport, optionJSON, optionYAML } from '@models/options';
 import { deepFreeze } from '@helpers/deep-freeze';
 
 export const functionWorkspace: Workspace = deepFreeze<Workspace>({
     enabled: true,
     type: WORKSPACE.FUNC,
     title: 'Function',
-    description: 'In the Function Workspace you can play around with Export or Def options. You can also change the output language',
+    description: 'In the Function Workspace you can play around with the Export, Eval, or Def options. You can also change the output language',
     icon: 'workspace-function',
     config: {
         func: {
             enabled: true,
             title: 'Function',
-            options: [ optionExport, optionDef ],
-            selected: optionExport,
+            options: [ optionExport, optionEval, optionDef ],
+            selected: optionEval,
         },
         inputTabs: [ {
             type: OPTION_TYPE.INPUT,
