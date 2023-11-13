222f798bed8b4240fe3cba2e929ddb34bd33404c
Paul Jolly <paul@myitcv.io>
2023-11-13 00:31:50 +0000

    preprocessor: add support for #commandComparator's
    In multi-step scripts, 'go test' can result in different output for
    otherwise identical inputs (i.e. in the case where we use --skipcache as
    part of CI, but it otherwise would have been a cache hit). In the case
    of `go test`, the differences almost always originate from timing
    differences: two runs of a test rarely take exactly the same amount of
    time, especially on a loaded machine (which is the case when guides are
    being generated in parallel). The good thing, however, is that in such
    cases the differences are well defined and structured: we can explain in
    terms of regular expressions and other such transformations how/where
    such output will differ.
    
    This CL introduces comparators. Comparators can be defined to apply to
    commands in a multi-step script in the same way as sanitisers (but they
    apply at very different times): exact command, command prefix, and step
    name.
    
    A comparator normalizes a cmd's output to transformedOutput, replacing
    parts of output that are known to vary in particular ways with rand, a
    predefined pseudo-random value under the control of the caller. This
    allows for two such command outputs that differ only in known ways to be
    compared as bytes-equal after normalization.
    
    Comparators allow a forced re-run of a multi-step script (via
    --skipcache, in a situation where there would otherwise have been a
    cache hit) to not result in any changes to the corresponding
    gen_cache.cue file for a page. If the output from the re-run is
    identical to the cached output after the same sequence of comparators
    have been applied to both, then we can avoid writing the re-run output
    to the cache: what's in the case is "identical" modulo comparators.
    
    This CL introduces two comparators:
    
    * patternComparator
    * unstableLineOrderComparator
    
    A patternComparator normlalizes output by replacing matches of a regular
    expression pattern with a known pseudo-random string. An
    unstableLineOrderComparator lexicographically sorts the lines of output
    and as such is generally applied last.
    
    Documentation of the role that comparators play in the wider
    preprocessor flow will follow in a later CL. For now this commit message
    is hopefully sufficient and sufficiently complete.
    
    Preprocessor-No-Write-Cache: true
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I392e239d32ab5538c95094f755d16428e9788117

---
+++ internal/cmd/preprocessor/cmd/comparators.go
@@ -0,0 +1,110 @@
+// Copyright 2024 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+// A comparator normalizes a cmd's output to transformedOutput, replacing parts
+// of output that are known to vary in particular ways with rand, a predefined
+// pseudo-random value under the control of the caller. This allows for two
+// such command outputs that differ only in known ways to be compared as
+// bytes-equal after normalization.
+type comparator interface {
+	normalize(cmd *commandStmt, output string, rand string) (transformedOutput string, err error)
+}
+
+// A patternSanitiser replaces command output that matches
+// a regular expression pattern some some replacement text.
+type patternComparator struct {
+	patternKindAndCommand
+}
+
+func (p *patternComparator) normalize(cmd *commandStmt, output string, rand string) (string, error) {
+	if matched, err := p.matches(cmd); err != nil || !matched {
+		return "", err
+	}
+	return regexpReplaceRand(output, p.pattern, rand), nil
+}
+
+func regexpReplaceRand(input string, re *regexp.Regexp, rand string) string {
+	matches := re.FindAllStringSubmatchIndex(input, -1)
+
+	if len(matches) == 0 {
+		// No match implies not substitutions required
+		return input
+	}
+
+	var b strings.Builder
+	var i int
+
+	put := func(j int) {
+		b.WriteString(input[i:j])
+		i = j
+	}
+
+	// Some substitutions required
+	var start, end int
+	var subMatches []int
+	for _, match := range matches {
+		// TODO: handle the case of nested submatches - by not handling them. We only
+		// handle single-level submatches.
+
+		// The first two elements of the match will be the bounds of the pattern match.
+		start, end, subMatches = match[0], match[1], match[2:]
+		put(start)
+		if len(subMatches) == 0 {
+			// The pattern does not specify any submatches. Hence we replace the entire
+			// match by just writing rand
+			b.WriteString(rand)
+		} else {
+			var subStart, subEnd, subLastEnd int
+			for len(subMatches) > 0 {
+				subStart, subEnd, subMatches = subMatches[0], subMatches[1], subMatches[2:]
+				if subEnd <= subLastEnd {
+					// Skip the sub-submatch - we know they are not overlapping
+					continue
+				}
+				put(subStart)
+				b.WriteString(rand)
+				i = subEnd // skip the submatch - we wrote rand
+				subLastEnd = subEnd
+			}
+			b.WriteString(input[i:end])
+		}
+		i = end
+	}
+	b.WriteString(input[end:])
+	return b.String()
+}
+
+// An unstableLineOrderComparator lexigraphically sorts the lines
+// a regular expression pattern some some replacement text.
+type unstableLineOrderComparator struct {
+	kindAndCommand
+}
+
+func (u *unstableLineOrderComparator) normalize(cmd *commandStmt, output string, rand string) (string, error) {
+	if matched, err := u.matches(cmd); err != nil || !matched {
+		return "", err
+	}
+
+	lines := strings.Split(output, "\n")
+	sort.Strings(lines)
+	return strings.Join(lines, "\n"), nil
+}
