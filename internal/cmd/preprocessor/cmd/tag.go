30bcde2836fc98b851ea73eb96ff263eb14d8a3d
Paul Jolly <paul@myitcv.io>
2024-02-11 15:05:19 +0000

    preprocessor: add support for ellipsis sanitiser tag directive
    https://cuelang.org/cl/1176701 introduced support for a new ellipsis
    sanitiser. This CL introduces very rough support for an ellipsis
    sanitiser tag that can be specified as part of the doc comment for a
    statement in a script node.
    
    Preprocessor-No-Write-Cache: true
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: I2c99eeecd2bb4653f80dd1dd36205156b927d597

--- internal/cmd/preprocessor/cmd/tag.go
+++ internal/cmd/preprocessor/cmd/tag.go
@@ -14,6 +14,12 @@
 
 package cmd
 
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+)
+
 const (
 	// tagNorun is the tag used in a txtar-based directive like code or script
 	// to indicate that that node should not be run. For an upload directive, it
@@ -40,4 +46,27 @@ const (
 	//     #location top-left top-right bottom
 	//
 	tagLocation = "location"
+
+	tagEllipsis = "ellipsis"
 )
+
+func findTag(src []byte, key, arg string) (args []string, present bool, err error) {
+	prefix := "#" + key
+	if arg != "" {
+		prefix += "(" + arg + ")"
+	}
+	sc := bufio.NewScanner(bytes.NewReader(src))
+	lineNo := 1
+	for sc.Scan() {
+		line := bytes.TrimSpace(sc.Bytes())
+		if after, found := bytes.CutPrefix(bytes.TrimSpace(line), []byte(prefix)); found {
+			args, err := parseLineArgs(string(after))
+			if err != nil {
+				err = fmt.Errorf("%d %w", lineNo, err)
+			}
+			return args, true, err
+		}
+		lineNo++
+	}
+	return nil, false, nil
+}
