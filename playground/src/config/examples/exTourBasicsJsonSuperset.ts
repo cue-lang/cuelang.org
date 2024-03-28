32ea62c1baeae9acc2ed8d8c328bbaa721f4f754
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-03-28 09:06:04 +0000

    playground: add basic examples
    This adds a small set of representative examples to the playground,
    replacing the current invalid examples.
    
    They are placed in an indicative structure, with the "Introduction"
    category being intended to hold examples for the user who landed on the
    playground before reading any other part of the site, and the "Tour"
    category showing a couple of CUE examples taken directly from the
    on-page CUE in docs/tour/.
    
    The Tour category isn't expanded to include all the docs examples, yet,
    as both cue-lang/cue#2995 and cue-lang/cue#2900 need resolving to let us
    avoid spending a *lot* of time on manual content syncing.
    
    Formatting notes for as&when the examples are expanded, and if their
    generation is automated:
    
    - Because cue-lang/cue#722 also strikes the playground ("top-level
      comments are indented") when the playground's default of "cue eval" is
      selected, care has been taken to make sure that comments are used that
      don't trigger this bug (as it looks pretty poor for predefined
      examples to have this problem). Some relevant information:
      - only the first comment presented at the top of the "eval" output is
        affected
      - it doesn't affect comments which aren't included in the output, such
        as introductory comments which are separated by a blank line from
        the first field emitted
      - it doesn't affect fields with a struct or list value
      - it only affects single-line comments
      - it can affect a comment that's not the first line of the source, if
        it's attached to any definition of the first field that's present in
        the emitted output
    
    - Including links from tour-derived examples back to the relevant tour
      page feels like a win, but they need to be split over two comment
      lines or the end of the URL disappears from view. This also has the
      nice effect of pushing the URL to the second line of the editor, which
      makes the "Follow link (ctrl+click)" mouseover popup *not* get
      visually truncated by the top edge of the editor pane.
    
    Preview-Path: /play
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I374ee591f767bea3a0e07f29ce7825468661f42f

---
+++ playground/src/config/examples/exTourBasicsJsonSuperset.ts
@@ -0,0 +1,32 @@
+import { cloneDeep } from 'lodash';
+import { Example } from '@models/example';
+import { WORKSPACE, WorkspaceConfig } from '@models/workspace';
+import { functionWorkspace } from '@config/workspaces';
+import { OPTION_TYPE } from '@models/options';
+
+/*  In the future we can also let the pre-processor create examples based on one of the workspace config's
+    instead of doing it with typescript. Make sure the example follows the Example model and the config
+    is correct for the chosen workspace. */
+
+// Clone config because we don't want to change the original workspace config
+const config = cloneDeep<WorkspaceConfig>(functionWorkspace.config);
+
+export const exTourBasicsJsonSuperset: Example = {
+    slug: 'tour-basics-json-superset',
+    title: 'Basics: JSON Superset',
+    category: 'Tour',
+    workspace: WORKSPACE.FUNC,
+    workspaceConfig: {
+        ...config,
+        inputTabs: config.inputTabs.map((tab) => {
+            if (tab.type === OPTION_TYPE.INPUT) {
+                // This text is a manually sync'd copy of <basename>.cue, produced by
+		//   cue export text: <basename>.cue
+                // TODO: make this better (cue-lang/cue#2995)
+                tab.code = "// This example is explained at\n// https://cuelang.org/docs/tour/basics/json-superset/\n\nimport \"math\"\n\n// Simple labels don't\n// need to be quoted.\none:       1\ntwo:       2\npiPlusOne: math.Pi + 1\n\n// Field names must be quoted if they contain\n// special characters, such as hyphen and space.\n\"quoted field names\": {\n\t\"two-and-a-half\":    2.5\n\t\"three point three\": 3.3\n\t\"four^four\":         math.Pow(4, 4)\n}\n\naList: [\n\t1,\n\t2,\n\t3,\n]\n";
+            }
+            return tab;
+        }),
+    },
+}
+
