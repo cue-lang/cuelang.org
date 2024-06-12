0d3053aff31e2428d8905176ce42f488ca351af8
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-06-12 10:05:47 +0100

    playground: remove About link from nav
    This removes the "About CUE Playground" link from the 3-dots nav in the
    playground because the link target currently 404s.
    
    Also, in the nav:
    - avoid a couple of redirects by linking to paths with trailing slashes
    - s/CUE home/CUE Homepage/
    
    Closes cue-lang/docs-and-content#158
    
    Preview-Path: /play/
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: If0addd7130705b4c39487caf80fbfeede59ebd89

--- playground/src/config/nav.ts
+++ playground/src/config/nav.ts
@@ -2,11 +2,12 @@ import { NavItem, TARGET_TYPE } from '@models/nav';
 import { environment } from 'environment';
 
 export const mainNav: NavItem[] = [
-    { url: '/', title: 'CUE home' },
-    { url: '/about-play', title: 'About CUE Playground' },
-    { url: '/docs', title: 'Documentation' },
+    { url: '/', title: 'CUE Homepage' },
+    // TODO: reinstate About link when page is available (https://github.com/cue-lang/docs-and-content/issues/159)
+    //{ url: '/about-play', title: 'About CUE Playground' },
+    { url: '/docs/', title: 'Documentation' },
     { url: 'https://github.com/cue-lang/cue/releases', title: 'Download CUE' },
-    { url: '/community', title: 'Community' },
+    { url: '/community/', title: 'Community' },
     { url: 'https://github.com/cue-lang/cue', title: 'Github' },
     { url: environment.issueUrl, title: 'Report an issue', target: TARGET_TYPE.BLANK },
 ];
