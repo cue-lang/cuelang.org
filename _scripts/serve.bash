7fb73bd0bce7364171030e47725e7375e46d9b02
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-06-13 16:01:17 +0100

    _scripts/serve.bash: fix startup race condition
    This works around a race condition caused by the preprocessor and Hugo's
    interaction on server startup.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I631e4c5e588df083f3cc2512b6d6679cf0b442d7

--- _scripts/serve.bash
+++ _scripts/serve.bash
@@ -5,6 +5,18 @@ set -eux
 # cd to the parent directory to that containing the script
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
 
+# Run the preprocessor once, before starting the server, so that the initial FS
+# state observed by Hugo (in hugo/) matches what's in content/.
+# This works around the following race condition:
+# - make a Hugo-breaking update in content/ that requires an entire
+#   preprocessor+server restart, as tracked in https://github.com/cue-lang/cue/issues/3109
+# - stop the server
+# - fix the issue in content/
+# - start the server
+# - Hugo processes hugo/ before the preprocessor has updated hugo/ with the fixed content
+# - Hugo exits, causing the preprocessor to exit
+_scripts/runPreprocessor.bash execute
+
 # Run the preprocess in serve mode which will itself run hugo.
 # Do not render to disk, to avoid any removed files lingering
 # in local serve mode (hugo does not tidy these with default
