ec6c47af9210b911c8453c73aa2bc0603bdd0831
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-07-12 17:35:11 +0100

    preprocessor: do not try to format .md files
    Like .txt and .csv files, we don't have to do anything when we see a
    Markdown file.
    
    Ignore so that we do not get warnings when running the preprocessor.
    
    Lazily not adding a test for this, as per commit
    72d3f0e49d38379eeac4665214a134e6b89132de
    
    Preprocessor-No-Write-Cache: true
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ie03ea1d8e3c560f9cfab742c902e147e1618ce43

--- internal/cmd/preprocessor/cmd/txtar_node.go
+++ internal/cmd/preprocessor/cmd/txtar_node.go
@@ -312,7 +312,7 @@ func (t *txtarRunContext) formatFiles() error {
 		case "proto":
 			// TODO: add support for proto formatting, after working out how to make
 			// this tooling generally available in the base docker image.
-		case "txt", "csv":
+		case "txt", "csv", "md":
 			// do nothing, simply avoid logging
 		default:
 			t.logf("%v: skipping formatting of file %q; unknown extension %q", t, a.Basename, a.Ext)
