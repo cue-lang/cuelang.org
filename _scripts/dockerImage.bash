166cace8e7e8bb6894d884a087363b8839388e9e
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-07-31 17:13:29 +0100

    deps: install Maven inside the Docker container
    This copies Apache Maven from the official Docker Hub "maven" container
    image into the preprocessor's Docker container.
    
    This change manually adapts the steps encoded in the Dockerfile that
    builds Maven's container image targetting the specific Java JDK that
    we're using:
    https://github.com/carlossg/docker-maven/blob/main/eclipse-temurin-22/Dockerfile.
    Whilst this URL might look non-official, it is linked to from the
    "3-eclipse-temurin-22" tag on https://hub.docker.com/_/maven.
    
    The version is fixed to a specific tag but not a digest (broadly echoing
    our consumption of the Golang container image) so that we can observe
    how much churn and/or how many problems result from allowing the version
    to drift forwards as issues are discovered and fixed in the image.
    
    The Maven binaries are added to the container's PATH in two different
    places, as noted in a comment in the Dockerfile and cue-lang/cue#2629.
    
    The preprocessor's Docker tag calculation is updated to include the
    contents of files matching internal/cmd/preprocessor/cmd/_docker/*,
    instead of the previously static list of files. This glob was added to
    ensure that a Maven-related file, newly added to the directory, was
    included in the tag's calculation. Whilst this file is no longer present
    in the current change, the update to the script is included, as the
    static list was already outdated before this change because it hadn't
    been updated to include testscript.sh.
    
    Preprocessor-No-Write-Cache: true
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I70b2df812bd9caebe1db29641d0c157b32188ab0

--- _scripts/dockerImage.bash
+++ _scripts/dockerImage.bash
@@ -2,5 +2,5 @@
 
 function dockerImageTag() {
 	root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
-	echo "preprocessor:$(cat $root/internal/cmd/preprocessor/cmd/_docker/Dockerfile $root/internal/cmd/preprocessor/cmd/_docker/entrypoint.sh | sha256sum | awk '{print $1}')"
+	echo "preprocessor:$(cat $root/internal/cmd/preprocessor/cmd/_docker/* | sha256sum | awk '{print $1}')"
 }
