353d793233674dad563eb838d458d54f029ab408
Paul Jolly <paul@myitcv.io>
2024-09-05 13:30:32 +0100

    _scripts: use latest staticcheck@v0.5.1
    Signed-off-by: Paul Jolly <paul@myitcv.io>
    Change-Id: Iee5f277176270a0970b5e65236c7dca31819f779
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/1200741
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

--- _scripts/staticcheck.bash
+++ _scripts/staticcheck.bash
@@ -2,7 +2,7 @@
 
 set -eu
 
-go run honnef.co/go/tools/cmd/staticcheck@v0.5.0 $(go list ./... |
+go run honnef.co/go/tools/cmd/staticcheck@v0.5.1 $(go list ./... |
 	grep -v github.com/cue-lang/cuelang.org/playground/internal/cuelang_org_go_internal |
 	grep -v github.com/cue-lang/cuelang.org/internal/parse
 )
