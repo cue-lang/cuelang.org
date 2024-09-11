625816688da7283490a13e4bb88c32905abafe8f
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-09-11 17:44:53 +0100

    internal/ci: deploy tip.cuelang.org
    This adds a job at the end of CI's trybot workflow that runs only in the
    main repo - either on the default branch, or when any commit is pushed
    to the ci/test branch. The job deploys tip.cuelang.org comprising the
    site as of the workflow's triggering commit, which is then:
    
    - modified using the stored patch that makes the site compatible with
      the tip of cue-lang/cue
    - configured to use the tip of cue-lang/cue for all named CUE versions
    - rebuilt using _scripts/regenPostInfraChange.bash
    - pushed to github:tip, which triggers a Netlify branch-build-and-deploy
    
    This change only adds the final item in the above list, and relies on
    the preceding CI jobs to have first performed the rest of the list
    successfully .
    
    Also: tidy up some of the preceding CI jobs' names.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ic97f59fd423eaa080684f0e45d314e8fb148e49e

---
+++ _scripts/tipDeploy.bash
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Usage:
+#     tipDeploy.bash <git-user-name> <git-user-email>
+#
+# tipDeploy commits the current state of the working tree and force-pushes it
+# to the "tip" branch, which triggers a build-and-deploy of tip.cuelang.org
+# inside Netlify.
+
+# cd to the parent directory to that containing the script
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+cd ..
+
+headShortRef=$(git rev-parse --short HEAD)
+gitUserName="$1"
+gitUserEmail="$2"
+gitRemote="${3:-origin}"
+
+git config user.name  "$gitUserName"
+git config user.email "$gitUserEmail"
+
+git add 'content/**/gen_cache.cue'
+git commit --allow-empty --message "tip.cuelang.org: $headShortRef: cache files"
+git add .
+git commit --allow-empty --message "tip.cuelang.org: $headShortRef: source changes"
+
+# A --force push is required because there's no automated *and meaningful* path
+# between the current state of the tip branch and the state we wish to push.
+# We don't lose any information by force pushing to this branch, whose only
+# purpose is to communicate the state that needs to be deployed and to act as a
+# trigger for the build inside Netlify.
+git push --force "$gitRemote" HEAD:tip
