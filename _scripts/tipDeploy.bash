b51b00a2937a3731cc01e68e7e08badd9626b34a
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-09-11 17:44:53 +0100

    internal/ci: deploy tip.cuelang.org
    This adds a job at the end of CI's trybot workflow that runs only in the
    main repo - either on the default branch, or when any commit is pushed
    to the ci/test branch. The job deploys tip.cuelang.org comprising the
    site as of the workflow's triggering commit, which is then:
    
    - modified using the stored patch that makes the site compatible with
      the tip of cue-lang/cue
    - configured to use the tip of cue-lang/cue for all named CUE versions
    - rebuilt using _scripts/regenPostInfraChange.bash
    - pushed to github:tip, which triggers a Netlify branch-build-and-deploy
    
    This change only adds the final item in the above list, and relies on
    previous CI jobs to have successfully performed the rest of the list.
    
    Because Netlify branch deploys are executed sequentially (per-push, not
    per-commit), the state of tip.cuelang.org will always reflect the latest
    cuelang.org content *unless* a manual push to the ci/test branch is
    performed. If ci/test receives new commits then the state of the site
    will reflect those commits (overlaid with the modifications detailed in
    the above list) until the next commit to cue-lang/cuelang.org:master,
    cue-lang/cue:master, or cue-lang/cuelang.org:ci/test prompts a
    redeployment. The most recent run "wins", so to speak, which is good
    enough for tip.cuelang.org given its intended purpose of providing
    build/render feedback to the project, and not providing authoritative
    documentation for a specific CUE version for the reader.
    
    Also: tidy up some of the previous CI jobs' names.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ic97f59fd423eaa080684f0e45d314e8fb148e49e
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/1201039
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>
    Reviewed-by: Paul Jolly <paul@myitcv.io>

---
+++ _scripts/tipDeploy.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Usage:
+#     tipDeploy.bash <git-user-name> <git-user-email>
+#
+# tipDeploy commits the current state of the working tree and force-pushes it
+# to the "tip" branch, which triggers a build-and-deploy of tip.cuelang.org
+# inside Netlify.
+
+# cd to the parent directory to that containing the script
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
+
+headShortRef=$(git rev-parse --short HEAD)
+gitUserName="$1"
+gitUserEmail="$2"
+gitRemote="${3:-origin}"
+
+git config user.name  "$gitUserName"
+git config user.email "$gitUserEmail"
+
+git add --verbose . ':^content/**/gen_cache.cue'
+git commit --verbose --allow-empty --message "tip.cuelang.org: site changes for $headShortRef" --trailer "Preprocessor-No-Write-Cache:true"
+git add --verbose .
+git commit --verbose --allow-empty --message "tip.cuelang.org: cache files for $headShortRef"
+
+# A --force push is required because there's no automated *and meaningful* path
+# between the current state of the tip branch and the state we wish to push.
+# We don't lose any information by force pushing to this branch, whose only
+# purpose is to communicate the state that needs to be deployed and to act as a
+# trigger for the build inside Netlify.
+git push --verbose --force "$gitRemote" HEAD:tip
