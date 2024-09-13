7021cafaed53b8690c85422f7dd4216773ae2a50
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-09-13 10:20:11 +0100

    _scripts: don't skip identical Netlify builds
    This updates _scripts/ignoreBuild.bash (the script that tells Netlify
    to skip a build for a specific commit) so that it doesn't fall through
    from our custom skip tests onto Netlify's default test. The default test
    is removed.
    
    Their default test checks if the pre-build content of the previous site
    deployment has any differences against the commit that triggered the
    current deployment. This technically saves a small amount of resource
    (around 90 seconds of deployment time), but it has been very infrequent
    until now. This doesn't impact CL previews, as they're not built inside
    Netlify, and will only affect builds of the "cue" Netlify site.
    
    This change is being made because of a few different factors which
    interact poorly. They've interacted infrequently until now, but they're
    about to become more frequent - prompting this change. These are the
    background factors:
    
    - Builds that are "skipped" by the ignoreBuild script trigger Netlify's
      "build *failed*" notifications. This is suboptimal, but has been
      tolerable until now because of the infrequency with which the skips
      occur.
    - Branch deploys (currently {www,tip}.cuelang.org) are almost never
      skipped because we embed information on each page (in the "Report an
      Issue" link) that is guaranteed to differ between each build,
      resulting in a non-zero git diff and the ignoreBuild script permitting
      the deployment.
    - Our recent update that redeploys tip.cuelang.org on every new commit
      on the default branches of cue-lang/cue and cue-lang/cuelang.org uses
      a push to cue-lang/cuelang.org:tip to prompt a Netlify branch deploy.
    
    This would be fine, if the only events that prompted this redeployment
    were those default branch commits. However, our daily "evict caches"
    scheduled workflow re-warms the caches by triggering the trybot on
    cue-lang/cuelang.org - which /finishes/ by pushing to
    cue-lang/cuelang.org:tip. This prompts the branch deployment of tip,
    which contains identical content to the most recent deployment, by
    design, and is dutifully skipped by ignoreBuild.bash - sending out
    nightly "failed deployment" emails.
    
    Our options to avoid these nightly emails include adding complexity into
    the trybot workflow: perhaps gating the push-to-tip on some visible sign
    of the triggering workflow having been scheduled.
    
    However, the change selected by this commit solves the problem by
    removing code and reducing complexity, whilst also being marginally more
    useful: it would allow us to manually trigger a re-deployment of the
    unchanged site via the Netlify UI in exceptional/unforeseen situations.
    
    Also: simplify the script's explanation of its exit code requirements,
    and add "set -o pipefail" for increased bash safety.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ic34871c85b623885f4f00eff0f0d154cab9ee753
    Reviewed-on: https://review.gerrithub.io/c/cue-lang/cuelang.org/+/1201175
    Reviewed-by: Paul Jolly <paul@myitcv.io>
    TryBot-Result: CUEcueckoo <cueckoo@cuelang.org>

--- _scripts/ignoreBuild.bash
+++ _scripts/ignoreBuild.bash
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
+set -euxo pipefail
 
-set -eux
-
-# ignoreBuild.bash is a script that runs as part of the Netlify deploy step
-# (configured in netlify.toml via internal/ci/netlify) that determines whether
-# a given commit should be deployed or not.
-
-# The script follows the logic requirements described in
+# ignoreBuild.bash runs as part of the Netlify deploy step (configured in
+# netlify.toml via internal/ci/netlify) and determines whether a given commit
+# should be deployed or not.
 #
-# https://docs.netlify.com/configure-builds/ignore-builds/
+# It must exit 1 if the commit should be deployed (i.e. commit /not/ ignored)
+# and exit 0 if the commit should not be deployed (i.e. commit ignored).
 #
-# That is it must exit 1 if the commit should be deployed, i.e. not ignored,
-# and exit 0 in case the commit should be ignored, i.e. not deployed.
+# These logic requirements are described in
+# https://docs.netlify.com/configure-builds/ignore-builds/.
 
 # cd to the parent directory to that containing the script
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
@@ -23,5 +21,5 @@ if _scripts/noWriteCache.bash; then
 	exit 0
 fi
 
-# Fall through to the default Netlify logic documented in the above page:
-git diff --quiet $CACHED_COMMIT_REF $COMMIT_REF
+# Deploy by default
+exit 1
