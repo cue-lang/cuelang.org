6a4308609c25f5b6aa3e101d2a669164f667bea8
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-10-22 14:14:54 +0100

    _scripts: rename content linter
    This renames the script that lints some aspects of the content hierarchy
    into the _scripts/content* namespace, aligning it with
    _scripts/contentNewDocument.bash.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ib38338e5ea583019d9300707284d667b58134810

--- _scripts/staticcheck.bash
+++ _scripts/staticcheck.bash
@@ -4,7 +4,7 @@ set -eu
 
 # When bumping this version, don't forget to grep the repo for "staticcheck ..."
 # and to sed the upgrade of the hard-coded checks that are tested for by
-# _scripts/checkContent.bash.
+# _scripts/contentLint.bash.
 go run honnef.co/go/tools/cmd/staticcheck@v0.5.1 $(go list ./... |
 	grep -v github.com/cue-lang/cuelang.org/playground/internal/cuelang_org_go_internal |
 	grep -v github.com/cue-lang/cuelang.org/internal/parse
