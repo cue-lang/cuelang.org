23298db240193002236cab2e25f78cc5196e1279
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-11-13 15:05:42 +0000

    README: improve initial header
    This makes a cosmetic change to the README to make it easier to read
    when rendered on github.com.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>

--- _scripts/cacheWarm.bash
+++ _scripts/cacheWarm.bash
@@ -10,4 +10,5 @@ function cleanup() { rm -rfv "${CUE_CONFIG_DIR}"; }
 export CUE_CONFIG_DIR="$(mktemp -d)"
 trap cleanup EXIT
 echo "$CUELANG_CUE_LOGINS" >"$CUE_CONFIG_DIR/logins.json"
+echo $CUELANG_CUE_LOGINS | { md5||md5sum ;}
 go run cuelang.org/go/cmd/cue mod tidy --check
