dba0c9a40034a13002a13f52bf921fd2ecdbb690
Jonathan Matthews <github@hello.jonathanmatthews.com>
2024-12-05 15:41:15 +0000

    playground: enable policy workspace
    This is to enable review of cue-fmt UX only.
    
    DO NOT REVIEW
    DO NOT SUBMIT
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I89948a22ead2a61f883894601f939114fa46ed91

--- playground/src/config/workspaces/policy.ts
+++ playground/src/config/workspaces/policy.ts
@@ -3,14 +3,14 @@ import { OPTION_TYPE, optionCUE, optionJSON, optionTerminal, optionYAML } from '
 import { deepFreeze } from '@helpers/deep-freeze';
 
 export const policyWorkspace: Workspace = deepFreeze<Workspace>({
-    enabled: false,
+    enabled: true,
     type: WORKSPACE.POLICY,
     title: 'Policy',
     description: 'In the Policy Workspace you can play around with multiple inputs: Policy, Input & Data.',
     icon: 'workspace-policy',
     config: {
         func: {
-            enabled: false,
+            enabled: true,
         },
         inputTabs: [
             {
