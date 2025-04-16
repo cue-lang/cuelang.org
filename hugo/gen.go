abf128583a50111a196de741206a8d0e9170d403
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-04-16 13:19:04 +0100

    _public: record robots.txt,_redirects in git
    This un-gitignores the final contents of the robots.txt file that the
    site will serve, along with the _redirects file that directly configures
    Netlify's server-side HTTP redirects.
    
    This will allow both files to be part of code reviews, which is becoming
    more important as we modify these files to satisfy the demands of
    various search engines.
    
    As part of this change the variable contents of robots.txt that points
    towards the sitemap is hardcoded and made static, pointing towards the
    production sitemap.
    
    This is acceptable because we never want a non-production site to be
    crawled (e.g. CL previews, PR previews, tip). This will either have the
    effect of causing a search engine to ignore the Sitemap: pointer on
    non-production sites (because the spec[0] says that this field must
    include an FQDN, and it's likely that they'll ignore a pointer to an
    unrelated site); or (less likely) will cause the search engine to crawl
    the production site instead - which would be a positive outcome,
    increasing the production site's crawl frequency as its sitemap's URLs
    contain the production FQDN.
    
    We also need to make NPM's package installation earlier inside CI, so it
    runs before go:generate. This is needed because this change now invokes
    Hugo as part of go:generate, and Hugo fails if some NPM packages aren't
    available.
    
    [0]: https://www.sitemaps.org/protocol.html#submit_robots
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ia8a035a3e4b2c785e552572f3e8dd7cb57f915ea

---
+++ hugo/gen.go
@@ -0,0 +1,19 @@
+// Copyright 2025 The CUE Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ci
+
+// Perform as little work as possible (== build fewer "kinds") whilst still
+// rendering the contents of _public/ that are tracked in git.
+//go:generate hugo --quiet --disableKinds page,section,term,rss,taxonomy --environment production
