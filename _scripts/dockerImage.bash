ae4b70e454e18d7915c26bc2be2fcc80dd97f2f5
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-08-05 09:30:18 +0100

    _scripts: push preprocessor image to Namespace registry
    DO NOT SUBMIT
    This is a test that pushes unconditionally.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: Ib21b851226c648e84e29122951979732148a67d1

--- _scripts/dockerImage.bash
+++ _scripts/dockerImage.bash
@@ -1,6 +1,10 @@
 # A "library" of functions that are common to Docker-related
 
 function dockerImageTag() {
+    echo "preprocessor:$(dockerImageHash)"
+}
+
+function dockerImageHash() {
 	root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/.."
-	echo "preprocessor:$(cat $root/internal/cmd/preprocessor/cmd/_docker/* | sha256sum | awk '{print $1}')"
+	cat $root/internal/cmd/preprocessor/cmd/_docker/* | sha256sum | awk '{print $1}'
 }
