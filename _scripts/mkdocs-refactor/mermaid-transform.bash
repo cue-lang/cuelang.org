a96f89b0509fc30f24e6578a1391934e32e56292
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-09-23 19:04:32 +0100

    mkdocs: replace Hugo mermaid shortcode with code block with mkdocs-refactor/mermaid-transform.bash
    This adapts /docs/ for MkDocs by replacing all uses of Hugo's mermaid
    shortcode with a mermaid fenced code block, and configures MkDocs to
    parse this specific fence.
    
    Diagram captions are turned into titles, which appears to be the only
    native labelling solution available. Follow ups can change any
    unacceptable titles into leading or trailing prose (or abandon the title
    text entirely) as appropriate.
    
    DO NOT SUBMIT
    ... without updating the commit message subject and body.
    
    Preview-Path: /mkdocs/docs/tutorial/working-with-a-custom-module-registry/
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I2ceb9ae0c2129b8a869541ca76aa3617b662317c

---
+++ _scripts/mkdocs-refactor/mermaid-transform.bash
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# cd to the grandparent directory of the script.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../.."
+
+. _scripts/mkdocs-refactor/config.env
+
+: "$mkdocsRoot" # Required.
+ppContent="$mkdocsRoot/preprocessor/content"
+
+# This rather gross multi-command sed seems to be forced by cross-platform
+# sed's handling of newlines.
+find "$ppContent" -name 'en.md' -exec sed -i.$$.bak -E \
+    -e ' # Handle the opening tag of a mermaid diagram without a caption.
+  s@^\{\{< mermaid >\}\}.*@``` mermaid@
+' \
+    -e ' # Handle the opening tag of a mermaid diagram with a caption.
+  /^\{\{< mermaid caption="[^"]+" >\}\}$/ {
+
+    # Insert two lines before the matched line
+    i\
+``` mermaid\
+---
+
+    # Replace the matched line with the dynamic title
+    s/^\{\{< mermaid caption="([^"]+)" >\}\}$/title: "\1"/
+
+    # Append a trailing line after the matched line
+    a\
+---
+
+}
+' \
+    -e '
+  s@^\{\{< /mermaid >\}\}.*@```@
+' \
+    {} +
+# Cater for BSD sed, which doesn't allow `-i` without a suffix.
+find "$ppContent" -type f -name "*.$$.bak" -delete
+
+_scripts/buildMkDocs.bash
