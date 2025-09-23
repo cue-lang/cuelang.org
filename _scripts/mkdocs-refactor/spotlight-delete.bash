5c4f1864db52d722d7196d411e15d93c8320b3bc
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-09-23 17:46:23 +0100

    mkdocs: remove Hugo spotlight shortcode with mkdocs-refactor/spotlight-delete.bash
    This adapts /docs/ for MkDocs by abandoning Hugo's spotlight shortcode.
    There's only one use, which renders acceptably when the shortcode's
    opening and closing tags are removed.
    
    DO NOT SUBMIT
    ... without updating the commit message subject and body.
    
    Preview-Path: /mkdocs/docs/howto/encode-json-yaml-with-cue/
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I8549347213abb020b350bd4387d75cb5cbf81121

---
+++ _scripts/mkdocs-refactor/spotlight-delete.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# cd to the grandparent directory of the script.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../.."
+
+. _scripts/mkdocs-refactor/config.env
+
+: "$mkdocsRoot" # Required.
+ppContent="$mkdocsRoot/preprocessor/content"
+
+find "$ppContent" -name 'en.md' -exec sed -i.$$.bak -E '
+  /^[[:space:]]*\{\{< spotlight >\}\}[[:space:]]*$/d
+  /^[[:space:]]*\{\{< \/spotlight >\}\}[[:space:]]*$/d
+' {} +
+# Cater for BSD sed, which doesn't allow `-i` without a suffix.
+find "$ppContent" -type f -name "*.$$.bak" -delete
+
+_scripts/buildMkDocs.bash
