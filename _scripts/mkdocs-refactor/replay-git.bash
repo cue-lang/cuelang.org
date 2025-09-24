28229fc26f55ee459f54470c7b48151063885162
Jonathan Matthews <github@hello.jonathanmatthews.com>
2025-09-24 13:56:08 +0100

    mkdocs: permit mid-refactor regeneration of content
    This sets up a small system that can be invoked with a git-rebase-i-x.
    It checks if the final word of the current commit's message is an
    executable in _scripts. If so, the working tree is reset to HEAD^, and
    the executable is invoked.
    
    It's expected that the result of this invocation is the successful
    regeneration of exactly the commit's existing state. This permits
    automated updates of the stack with
    
        git rebase -i --exec _scripts/mkdocs-refactor/replay-git.bash
    
    ... which will stop if the transformation script fails, or if it leaves
    any untracked or modified files in the working tree that should be
    examined manually.
    
    DO NOT SUBMIT
    until this commit message has been updated.
    
    DO NOT SUBMIT
    unless we either remove this script's containing directory later in the
    MkDocs stack, or we want this script to persist past the stack for some
    reason.
    
    Signed-off-by: Jonathan Matthews <github@hello.jonathanmatthews.com>
    Change-Id: I44c36b9260e6d26064fbd9bc3c1c5a268c859b79

---
+++ _scripts/mkdocs-refactor/replay-git.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# cd to the grandparent directory of the script.
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../.."
+
+possibleScript="_scripts/$(git log -1 --pretty=%s | awk "{print \$NF}")"
+if test -x "$possibleScript"
+then
+    # Move the repo back to the state before the current commit, except for the _scripts dir.
+    git restore --source=HEAD^ -- . ':(exclude)_scripts/' ':(exclude)mkdocs/config.yml'
+
+    # Invoke the not-rolled-back script.
+    "$possibleScript"
+
+fi
